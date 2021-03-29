1fe5323341dac2d82517e9be2d981ba195a4b505
bls20AWS <benjasl@a483e7468bf4.ant.amazon.com>
2021-03-29 16:53:21 +0100

    s3 examples

--- s3-lambda/README.md
+++ s3-lambda/README.md
@@ -2,9 +2,9 @@
 
 This pattern is a Lambda function asynchronously triggered when an object is uploaded to an S3 bucket. 
 
-The SAM template deploys a Lambda function, an S3 bucket and the IAM resources required to ru the application. A Lambda function consumes `ObjectCreated` events from an Amazon S3 bucket. The Lambda code checks the uploaded file is an image and creates a thumbail version of the image in the same bucket.
+The SAM template deploys a Lambda function, an S3 bucket and the IAM resources required to run the application. A Lambda function consumes `ObjectCreated` events from an Amazon S3 bucket. The Lambda code checks the uploaded file is an image and creates a thumbail version of the image in the same bucket.
 
-Learn more about this pattern at Serverless Land Patterns: https://serverlessland.com/patterns/s3-lambda
+Learn more about this pattern at Serverless Land Patterns: [https://serverlessland.com/patterns/s3-lambda](https://serverlessland.com/patterns/s3-lambda)
 
 Important: this application uses various AWS services and there are costs associated with these services after the Free Tier usage - please see the [AWS Pricing page](https://aws.amazon.com/pricing/) for details. You are responsible for any AWS costs incurred. No warranty is implied in this example.
 
@@ -27,6 +27,7 @@ Important: this application uses various AWS services and there are costs associ
 
 4. From the command line, run:
 ```
+sam build
 sam deploy --guided
 ```
 Choose a stack name, select the desired AWS Region, and allow SAM to create roles with the required permissions. Once you have run guided mode once, you can use `sam deploy` in future to use these defaults.
@@ -41,16 +42,28 @@ Choose a stack name, select the desired AWS Region, and allow SAM to create role
 
 ## Testing
 
-Edit the sample event data in the `/events/inputFile.txt` File. Providing your sourcebucket name and a .jpg object key. Note that the S3 bucket  name is provided by teh stack output after deployment.
-
-### Success Testing
-
-Run the following Lambda CLI invoke command to invoke the function. Note, you must edit the {FunctionArn} placeholder with the ARN of the deployed Lambda function. This is provided in the stack outputs.
+Run the following S3 CLI  command to upload an image to the S3 bucket. Note, you must edit the {SourceBucketName} placeholder with the name of the S3 Bucket. This is provided in the stack outputs.
 
 ```bash
-aws lambda invoke --function-name {FunctionArn} --invocation-type Event \
---payload file://inputFile.txt outputfile.txt
+aws s3 cp './events/exampleImage.png'  s3://{SourceBucketName}
 ```
+
+Run the following command to check that a new version of the image has been created in the destination bucket.
+
+```bash
+aws s3 ls s3://{DestinationBucketName}
+```
+
+## Cleanup
+
+1. Delete the stack
+
+aws cloudformation delete-stack --stack-name STACK_NAME
+
+1. Confirm the stack has been deleted
+
+aws cloudformation list-stacks --query "StackSummaries[?contains(StackName,'STACK_NAME')].StackStatus
+
 
 Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
--- s3-lambda/src/app.js
+++ s3-lambda/src/app.js
@@ -1,77 +1,52 @@
-// dependencies
-const AWS = require('aws-sdk');
-const util = require('util');
-const sharp = require('sharp');
+/*! Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: MIT-0
+ */
 
-// get reference to S3 client
-const s3 = new AWS.S3();
+const AWS = require('aws-sdk')
+AWS.config.update({ region: process.env.AWS_REGION })
+const s3 = new AWS.S3()
 
-exports.handler = async (event, context, callback) => {
+const gm = require('gm')
+const NEW_SIZE_PX = 480
 
-    // Read options from the event parameter.
-    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-    const srcBucket = event.Records[0].s3.bucket.name;
-    // Object key may have spaces or unicode non-ASCII characters.
-    const srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-    const dstBucket = srcBucket + "-resized";
-    const dstKey    = "resized-" + srcKey;
+// 1. Load the image from S3.
+// 2. Transform the image.
+// 3. Save the new image in another S3 bucket.
 
-    // Infer the image type from the file suffix.
-    const typeMatch = srcKey.match(/\.([^.]*)$/);
-    if (!typeMatch) {
-        console.log("Could not determine the image type.");
-        return;
-    }
+exports.handler = async (event) => {
+    // console.log(JSON.stringify(event, null, 2))
+    const Key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "))
 
-    // Check that the image type is supported  
-    const imageType = typeMatch[1].toLowerCase();
-    if (imageType != "jpg" && imageType != "png") {
-        console.log(`Unsupported image type: ${imageType}`);
-        return;
-    }
+    // Read the object from S3
+    const s3Object = await s3.getObject({
+        Bucket: event.Records[0].s3.bucket.name,
+        Key
+    }).promise()
 
-    // Download the image from the S3 source bucket. 
+    // Resize the image
+    const data = await resizeImage(s3Object.Body)
+    
+    // Write to S3    
+    const result = await s3.putObject({
+        Bucket: process.env.DESTINATION_BUCKETNAME,
+        Key,
+        ContentType: 'image/jpeg',
+        Body: data,
+        ACL: 'public-read'
+    }).promise()
+    console.log('Write result: ', result)
+}
 
-    try {
-        const params = {
-            Bucket: srcBucket,
-            Key: srcKey
-        };
-        var origimage = await s3.getObject(params).promise();
 
-    } catch (error) {
-        console.log(error);
-        return;
-    }  
 
-    // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
-    const width  = 200;
-
-    // Use the Sharp module to resize the image and save in a buffer.
-    try { 
-        var buffer = await sharp(origimage.Body).resize(width).toBuffer();
-            
-    } catch (error) {
-        console.log(error);
-        return;
-    } 
-
-    // Upload the thumbnail image to the destination bucket
-    try {
-        const destparams = {
-            Bucket: dstBucket,
-            Key: dstKey,
-            Body: buffer,
-            ContentType: "image"
-        };
-
-        const putResult = await s3.putObject(destparams).promise(); 
-        
-    } catch (error) {
-        console.log(error);
-        return;
-    } 
-        
-    console.log('Successfully resized ' + srcBucket + '/' + srcKey +
-        ' and uploaded to ' + dstBucket + '/' + dstKey);
-};
+// Resize - takes and returns a image buffer
+const resizeImage = async (buffer) => {
+    return new Promise((resolve, reject) => {
+    gm(buffer)
+        .resize(NEW_SIZE_PX, NEW_SIZE_PX)
+        .toBuffer('jpg', function (err, data) {
+            if (err) return reject(err)
+            resolve(data)
+        })
+    })
+}
