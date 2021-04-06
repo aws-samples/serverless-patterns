53d89ce6fcf14e48460dd2e0df7f1a2a7c619e40
gadavis2 <gadavis2@gmail.com>
2021-04-06 16:47:58 -0500

    Added actual error message

--- lambda-aurora-serverless/src/app.js
+++ lambda-aurora-serverless/src/app.js
@@ -100,6 +100,6 @@ exports.handler = async (event, context) => {
     return response;
   } catch (error) {
     console.error(error);
-    throw new Error("Function Error");
+    throw new Error(error);
   }
 };
