fc5dfee1b1f9fa2af00ad33e474df674264aa9dd
Beswick <jbeswick@amazon.com>
2021-04-16 08:11:25 -0400

    Updated URLs

--- lambda-ssm-parameter/src/app.js
+++ lambda-ssm-parameter/src/app.js
@@ -5,25 +5,28 @@
 // 1. Receive event with method and body.
 // 2. GET or PUT an SSM Parameter Store parameter.
 // 3. Return a response with parameter result.
-const AWS = require("aws-sdk");
-const ssm = new AWS.SSM();
+
+const AWS = require("aws-sdk")
+const ssm = new AWS.SSM()
+
 exports.handler = async (event, context) => {
   try {
     // Log event and context object to CloudWatch Logs
-    console.log("Event:\n", JSON.stringify(event, null, 2));
-    console.log("Context:\n", JSON.stringify(context, null, 2));
+    console.log("Event:\n", JSON.stringify(event, null, 2))
+    console.log("Context:\n", JSON.stringify(context, null, 2))
+
     // Log parameter name from environment variables
     const parameterName = process.env.SSMParameterName;
     console.log("SSMParameterName: ", parameterName);
 
     // Get method and body from event
-    const method = event.method;
-    const body = event.body;
-    console.log("Method: ", method);
-    console.log("Body: ", body);
+    const method = event.method
+    const body = event.body
+    console.log("Method: ", method)
+    console.log("Body: ", body)
 
     // Modify or Get parameter
-    let result = "";
+    let result = ""
     if (method == "PUT") {
       const ssmPutParams = {
         Name: parameterName,
@@ -31,24 +34,27 @@ exports.handler = async (event, context) => {
         Overwrite: true,
         Type: "String",
       };
-      result = await ssm.putParameter(ssmPutParams).promise();
+      result = await ssm.putParameter(ssmPutParams).promise()
     } else if (method == "GET") {
       const ssmGetParams = {
         Name: parameterName,
       };
-      result = await ssm.getParameter(ssmGetParams).promise();
+      result = await ssm.getParameter(ssmGetParams).promise()
     } else {
       result = "Method not supported"
     }
+
     console.log("Result:\n", JSON.stringify(result));
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result),
-    };
-    return response;
+      body: JSON.stringify(result)
+    }
+
+    return response
+
   } catch (error) {
     console.error(error);
     throw new Error(error);
   }
-};
+}
