c621b2fb1b325b6fb2db15a02255acbc68d8621c
Ami Patel <apateamz@amazon.com>
2021-06-25 15:35:15 -0500

    Updated response type for put events

--- appsync-eventbridge/cdk/lib/appsync-eventbridge-stack.ts
+++ appsync-eventbridge/cdk/lib/appsync-eventbridge-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from '@aws-cdk/core';
-import { GraphqlApi, Schema, FieldLogLevel, AuthorizationType, CfnDataSource, CfnResolver, MappingTemplate } from '@aws-cdk/aws-appsync';
+import { GraphqlApi, Schema, FieldLogLevel, AuthorizationType, MappingTemplate } from '@aws-cdk/aws-appsync';
 import { Role, ServicePrincipal, PolicyStatement } from "@aws-cdk/aws-iam";
 import {EventBus} from "@aws-cdk/aws-events";
-import { join } from 'path'
+import { join } from 'path';
 
 export class AppsyncEventbridgeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -31,68 +31,19 @@ export class AppsyncEventbridgeStack extends cdk.Stack {
         actions: ["events:PutEvents"]
       })
     );
-    const endpoint = "https://events." + this.region + ".amazonaws.com/";
-    const httpdatasource = new CfnDataSource(this, 'httpdatasource', {
-        apiId: api.apiId,
-        name: "EventBridgeDataSource",
-        type: "HTTP",
-        httpConfig:{
-          authorizationConfig: {
-            authorizationType: "AWS_IAM",
-              awsIamConfig: {
-                signingRegion: this.region,
-                signingServiceName: "events"
-              }
-          },
-          endpoint: "https://events." + this.region + ".amazonaws.com/",
-        },
-        serviceRoleArn: appsyncEventBridgeRole.roleArn
-      }
-    );
-    //const datasource = api.addHttpDataSource(id: 'EventBridgeDataSource', endpoint: endpoint, );
 
-    const putEventResolver = new CfnResolver(this, "PutEventMutationResolver", {
-      apiId: api.apiId,
-      typeName: "Mutation",
-      fieldName: "putEvent",
-      dataSourceName: httpdatasource.name,
-      requestMappingTemplate: `{
-        "version": "2018-05-29",
-        "method": "POST",
-        "resourcePath": "/",
-        "params": {
-          "headers": {
-            "content-type": "application/x-amz-json-1.1",
-            "x-amz-target":"AWSEvents.PutEvents"
-          },
-          "body": {
-            "Entries":[
-              {
-                "Source":"appsync",
-                "EventBusName": "default",
-                "Detail":"{ \\\"event\\\": \\\"$ctx.arguments.event\\\"}",
-                "DetailType":"Event Bridge via GraphQL"
-               }
-            ]
-          }
-        }
-      }`,
-      responseMappingTemplate: `## Raise a GraphQL field error in case of a datasource invocation error
-      #if($ctx.error)
-        $util.error($ctx.error.message, $ctx.error.type)
-      #end
-      ## if the response status code is not 200, then return an error. Else return the body **
-      #if($ctx.result.statusCode == 200)
-          ## If response is 200, return the body.
-          {
-            "result": "$util.parseJson($ctx.result.body)"
-          }
-      #else
-          ## If response is not 200, append the response to error block.
-          $utils.appendError($ctx.result.body, $ctx.result.statusCode)
-      #end`
+    const endpoint = "https://events." + this.region + ".amazonaws.com/";
+    const httpdatasource = api.addHttpDataSource('events', endpoint, {
+      authorizationConfig: { signingRegion: this.region, signingServiceName: 'events' },
     });
-    putEventResolver.addDependsOn(httpdatasource);
+    eventBus.grantPutEventsTo(httpdatasource.grantPrincipal);
+
+    httpdatasource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'putEvent',
+      requestMappingTemplate: MappingTemplate.fromFile(join(__dirname, 'request.vtl')),
+      responseMappingTemplate: MappingTemplate.fromFile(join(__dirname, 'response.vtl'))
+    })
 
     new cdk.CfnOutput(this, 'graphqlUrl', { value: api.graphqlUrl })
     new cdk.CfnOutput(this, 'apiKey', { value: api.apiKey! })
