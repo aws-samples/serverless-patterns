1cf9d643b151763c11ce1c035f1c615453bca073
GlennChia <glennchia7@gmail.com>
2021-09-24 15:26:07 +0800

    feat: add dynamodb table

--- lambda-dynamodb-cdk/cdk/lib/cdk-stack.ts
+++ lambda-dynamodb-cdk/cdk/lib/cdk-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import { Table, BillingMode, AttributeType } from '@aws-cdk/aws-dynamodb';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB Table
+    const dynamoTable = new Table(this, 'DynamoTable', {
+      partitionKey: {name:'ID', type: AttributeType.STRING},
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
   }
 }
