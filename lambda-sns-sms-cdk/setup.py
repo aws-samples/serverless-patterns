ee5241086ab5103c025e836c8ac5cab979bfeff4
Ian Larson <illarso@amazon.com>
2021-09-24 17:24:14 -0700

    fixed CDK version number and cleaned up unneccesary files

--- lambda-sns-sms-cdk/setup.py
+++ lambda-sns-sms-cdk/setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="lambda_sns_sms_cdk"),
 
     install_requires=[
-        "aws-cdk.core==1.121.0",
+        "aws-cdk.core==1.124.0",
     ],
 
     python_requires=">=3.6",
