6a512a1f3c6397f7c45fc4a8fed19f12cdb25b1a
GlennChia <glennchia7@gmail.com>
2021-10-16 12:39:06 +0000

    build: cdk init

---
+++ fargate-cdk/cdk/test/cdk.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Cdk from '../lib/cdk-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Cdk.CdkStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
