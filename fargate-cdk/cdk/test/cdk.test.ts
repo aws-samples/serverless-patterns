6ad4e99eed0d00659a411345c469739cda52ed0f
GlennChia <glennchia7@gmail.com>
2021-10-16 13:35:34 +0000

    test: check for core resources

--- fargate-cdk/cdk/test/cdk.test.ts
+++ fargate-cdk/cdk/test/cdk.test.ts
@@ -1,13 +1,13 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as Cdk from '../lib/cdk-stack';
+import { CdkStack } from '../lib/cdk-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Cdk.CdkStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('Validate stack resources', () => {
+  const app = new cdk.App();
+  const stack = new CdkStack(app, 'MyTestStack');
+
+  expectCDK(stack).to(haveResource('AWS::ECS::Cluster'));
+  expectCDK(stack).to(haveResource('AWS::ECS::TaskDefinition'));
+  expectCDK(stack).to(haveResource('AWS::ECS::Service'));
+  expectCDK(stack).to(haveResource('AWS::ElasticLoadBalancingV2::LoadBalancer'));
 });
