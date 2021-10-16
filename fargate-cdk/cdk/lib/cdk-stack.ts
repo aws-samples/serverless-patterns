1d68142f6c01a677112c07ac12f53d17e5511aa5
GlennChia <glennchia7@gmail.com>
2021-10-16 12:44:20 +0000

    feat: add base script from aws docs

--- fargate-cdk/cdk/lib/cdk-stack.ts
+++ fargate-cdk/cdk/lib/cdk-stack.ts
@@ -1,9 +1,28 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 3
+    });
+
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
+    });
+
+    const fargate = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      },
+      memoryLimitMiB: 2048,
+    });
   }
 }
