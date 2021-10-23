28450c23b8f0e0b8668d8ae67c7266dbe5a6b37e
Adilson Perinei <adilson@perinei.com>
2021-10-22 20:43:17 -0400

    removing duplicated files

--- eventbridge-scheduled-lambda-cdk-typescript/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-console.log("Starting...")
-exports.handler = (event, context, callback) => {
-    let interval_in_minutes = process.env.interval_in_minutes
-    console.log("This function runs every " + interval_in_minutes + " minutes")
-    callback(null, 'Complete - OK');
-};
