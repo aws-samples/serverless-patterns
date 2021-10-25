11ce5e43ab834c997ef360891d8bb5652c54c775
GlennChia <glennchia7@gmail.com>
2021-10-25 13:45:54 +0000

    feat: add private s3 bucket

--- fargate-s3-cdk/cdk/lib/cdk-stack.ts
+++ fargate-s3-cdk/cdk/lib/cdk-stack.ts
@@ -1,9 +1,17 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const demoBucket = new s3.Bucket(this, 'demoBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      bucketName: 'demo-bucket-serverless-patterns',
+      encryption: s3.BucketEncryption.KMS_MANAGED,
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: false,  // turn off versioning to make cdk destroy easier
+    });
   }
 }
