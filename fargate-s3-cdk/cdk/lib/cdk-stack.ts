b04f1b3e76736651920ca2bb96694bfc6a9ce6a1
GlennChia <glennchia7@gmail.com>
2021-10-29 01:32:56 +0000

    feat: custom fargate image integration with s3
    uses built-in grantReadWrite permissions for read, list, write

--- fargate-s3-cdk/cdk/lib/cdk-stack.ts
+++ fargate-s3-cdk/cdk/lib/cdk-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
+import path = require('path');
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,5 +18,30 @@ export class CdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       versioned: false,  // turn off versioning to make cdk destroy easier
     });
+
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 3
+    });
+
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
+    });
+
+    const fargate = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset(path.join(__dirname, '../src/')),
+        environment: {
+          bucketName: demoBucket.bucketName,
+          region: process.env.CDK_DEFAULT_REGION!
+        },
+      },
+      memoryLimitMiB: 2048,
+    });
+
+    // Read and Write permissions for Fargate
+    demoBucket.grantReadWrite(fargate.taskDefinition.taskRole);
   }
 }
