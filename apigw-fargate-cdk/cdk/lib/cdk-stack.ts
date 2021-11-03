5452b0f892bfce1be431868b5eb7b075042d9c13
GlennChia <glennchia7@gmail.com>
2021-11-03 15:32:00 +0000

    feat: integrate apigw with alb fargate service

--- apigw-fargate-cdk/cdk/lib/cdk-stack.ts
+++ apigw-fargate-cdk/cdk/lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import { Vpc } from '@aws-cdk/aws-ec2';
 import { Cluster, ContainerImage } from '@aws-cdk/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
+import * as apig from '@aws-cdk/aws-apigatewayv2';
 import path = require('path');
 
 export class CdkStack extends cdk.Stack {
@@ -25,6 +26,42 @@ export class CdkStack extends cdk.Stack {
       },
       assignPublicIp: false,
       memoryLimitMiB: 2048,
+      publicLoadBalancer: false,
     });
+
+    const httpVpcLink = new cdk.CfnResource(this, 'HttpVpcLink', {
+      type: 'AWS::ApiGatewayV2::VpcLink',
+      properties: {
+        Name: 'V2 VPC Link',
+        SubnetIds: vpc.privateSubnets.map(m => m.subnetId)
+      }
+    });
+
+    const api = new apig.HttpApi(this, 'HttpApiGateway', {
+      apiName: 'ApigwFargate',
+      description: 'Integration between apigw and Application Load-Balanced Fargate Service',
+    });
+
+    const integration = new apig.CfnIntegration(this, 'HttpApiGatewayIntegration', {
+      apiId: api.httpApiId,
+      connectionId: httpVpcLink.ref,
+      connectionType: 'VPC_LINK',
+      description: 'API Integration with AWS Fargate Service',
+      integrationMethod: 'GET', // for GET and POST, use ANY
+      integrationType: 'HTTP_PROXY',
+      integrationUri: fargate.listener.listenerArn,
+      payloadFormatVersion: '1.0', // supported values for Lambda proxy integrations are 1.0 and 2.0. For all other integrations, 1.0 is the only supported value
+    });
+
+    new apig.CfnRoute(this, 'Route', {
+      apiId: api.httpApiId,
+      routeKey: 'GET /',  // for something more general use 'ANY /{proxy+}'
+      target: `integrations/${integration.ref}`,
+    })
+
+    new cdk.CfnOutput(this, 'APIGatewayUrl', {
+      description: 'API Gateway URL to access the GET endpoint',
+      value: api.url!
+    })
   }
 }
