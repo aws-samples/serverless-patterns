e379ce01408315658ea3e12710e169ae4ad57b8d
GlennChia <glennchia7@gmail.com>
2021-11-05 07:16:48 +0000

    feat: add standard sqs queue with encryption

--- fargate-sqs/cdk/lib/cdk-stack.ts
+++ fargate-sqs/cdk/lib/cdk-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import * as sqs from '@aws-cdk/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const sqsQueue = new sqs.Queue(this, 'Queue', {
+      encryption: sqs.QueueEncryption.KMS_MANAGED,
+    });
   }
 }
