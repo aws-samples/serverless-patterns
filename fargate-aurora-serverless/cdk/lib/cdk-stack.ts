68b515c096be9a6a141ef18dbee4788a1bf54bfc
GlennChia <glennchia7@gmail.com>
2021-11-17 06:35:08 +0000

    feat: enable data api on serverless cluster

--- fargate-aurora-serverless/cdk/lib/cdk-stack.ts
+++ fargate-aurora-serverless/cdk/lib/cdk-stack.ts
@@ -23,6 +23,7 @@ export class CdkStack extends cdk.Stack {
     });
 
     const auroraServerlessCluster = new rds.ServerlessCluster(this, 'AuroraServerlessCluster', {
+      enableDataApi: true,
       engine: rds.DatabaseClusterEngine.AURORA,
       credentials: rds.Credentials.fromSecret(databaseCredentialsSecret),
       vpc,
