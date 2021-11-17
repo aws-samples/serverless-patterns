1ba80b08e23c402dddc7d06b365fa6332d02180c
GlennChia <glennchia7@gmail.com>
2021-11-17 06:13:02 +0000

    feat: deploy aurora serverless cluster in vpc

--- fargate-aurora-serverless/cdk/lib/cdk-stack.ts
+++ fargate-aurora-serverless/cdk/lib/cdk-stack.ts
@@ -1,15 +1,23 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as rds from '@aws-cdk/aws-rds';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 3
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const auroraServerlessCluster = new rds.ServerlessCluster(this, 'AuroraServerlessCluster', {
+      engine: rds.DatabaseClusterEngine.AURORA,
+      vpc,
+      scaling: {
+        autoPause: cdk.Duration.minutes(10),
+        minCapacity: rds.AuroraCapacityUnit.ACU_1,
+        maxCapacity: rds.AuroraCapacityUnit.ACU_2,
+      }
+    });
   }
 }
