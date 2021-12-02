d0a241930a0d154c986cbd6522de5126af025c12
Paulo Heneine <paulhene@amazon.com>
2021-12-02 10:13:40 -0300

    Creating the CDK pattern SQS -> Lambda -> DynamoDB

---
+++ sqs-lambda-dynamodb-cdk/tests/unit/test_vsam_to_dynamo_stack.py
@@ -0,0 +1,32 @@
+from aws_cdk import (
+        core,
+        assertions
+    )
+
+from vsam_to_dynamo.vsam_to_dynamo_stack import VsamToDynamoStack
+
+
+def test_sqs_queue_created():
+    app = core.App()
+    stack = VsamToDynamoStack(app, "vsam-to-dynamo")
+    template = assertions.Template.from_stack(stack)
+
+    template.has_resource_properties("AWS::SQS::Queue", {
+        "VisibilityTimeout": 300
+    })
+
+
+def test_lambda_function_created():
+    app = core.App()
+    stack = VsamToDynamoStack(app, "vsam-to-dynamo")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::Lambda::Function", 1)
+
+
+def test_dynamodb_table_created():
+    app = core.App()
+    stack = VsamToDynamoStack(app, "vsam-to-dynamo")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::DynamoDB::Table", 1)
