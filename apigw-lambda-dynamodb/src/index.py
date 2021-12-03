d82f919d0aa731d420a058f9db81087248542ebf
Ravi Srinivasan <rssrnv@amazon.com>
2021-12-03 15:31:15 -0500

    Serverless pattern - API Gateway to Lambda to DynamoDB

---
+++ apigw-lambda-dynamodb/src/index.py
@@ -0,0 +1,12 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+import boto3
+
+def lambda_handler(event, context):
+  dynamodb_client = boto3.client('dynamodb')
+  dynamodb_client.put_item(TableName='WeatherData', Item={'id': {'S': '1'}, 'Weather': {'S': 'Sunny'}})
+  return {
+      'statusCode': 200,
+      'body': 'Successfully inserted data!'
+  }
