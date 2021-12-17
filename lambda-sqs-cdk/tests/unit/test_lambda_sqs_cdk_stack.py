0f7a38637697e25a1afa7d65630fb7d386907743
Jose Albino Neto <albinoneto@gmail.com>
2021-12-16 21:42:48 -0300

    create lambda sqs cdk pattern

---
+++ lambda-sqs-cdk/tests/unit/test_lambda_sqs_cdk_stack.py
@@ -0,0 +1,24 @@
+from aws_cdk import (
+        core,
+        assertions
+    )
+
+from lambda_sqs_cdk.lambda_sqs_cdk_stack import LambdaSqsCdkStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in lambda_sqs_cdk/lambda_sqs_cdk_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = LambdaSqsCdkStack(app, "lambda-sqs-cdk")
+    template = assertions.Template.from_stack(stack)
+
+    template.has_resource_properties("AWS::SQS::Queue", {
+        "VisibilityTimeout": 300
+    })
+
+def test_lambda_function_created():
+    app = core.App()
+    stack = LambdaSqsCdkStack(app, "lambda-sqs-cdk")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::Lambda::Function", 1)
