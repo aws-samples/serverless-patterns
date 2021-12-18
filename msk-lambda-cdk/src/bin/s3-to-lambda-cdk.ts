faadd4cf298d6b09f96cf782992975400fba77e8
Pablo Silva <pcsilva@amazon.com>
2021-12-18 09:18:49 -0300

    Initial commit

--- msk-lambda-cdk/src/bin/s3-to-lambda-cdk.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { MSKToLambdaCdkStack } from '../lib/msk-to-lambda-cdk-stack';
-
-const app = new cdk.App();
-new MSKToLambdaCdkStack(app, 'MSKToLambdaCdkStack');
--- msk-lambda-cdk/src/lambda-fns/readStream/handler.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-  event.Records.forEach((record) => {
-    console.log(`Event Name: ${JSON.stringify(record.eventName)}`);
-    // #TODO: Show record information
-    // console.log(`MSK Request: ${JSON.stringify(record.s3)}`);
-    
-  });
-};
