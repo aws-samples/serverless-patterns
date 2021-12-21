bc5b8ba3325357c186f111566badcf31ea54cb2d
Pablo Silva <pcsilva@amazon.com>
2021-12-21 18:56:49 -0300

    Update msk-lambda-cdk.ts

--- msk-lambda-cdk/bin/msk-lambda-cdk.ts
+++ msk-lambda-cdk/bin/msk-lambda-cdk.ts
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+/*! Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: MIT-0
+ */
+
 import * as cdk from '@aws-cdk/core';
 
 import { MskLambdaCdkStack } from '../lib/msk-lambda-cdk-stack';
