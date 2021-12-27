8ede748ea63ff577d65f070a256fff8180fac6c4
GlennChia <glennchia7@gmail.com>
2021-12-27 11:24:15 +0000

    feat: deploy sns topic

--- fargate-sns-cdk/cdk/lib/cdk-stack.ts
+++ fargate-sns-cdk/cdk/lib/cdk-stack.ts
@@ -1,16 +1,14 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Topic } from 'aws-cdk-lib/aws-sns';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const topic = new Topic(this, 'Topic', {
+      displayName: 'demo topic',
+      topicName: 'demoTopic',
+    });
   }
 }
