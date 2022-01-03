608661fe7c3879bab0afbce05a759e98d44fcce8
GlennChia <glennchia7@gmail.com>
2022-01-03 02:41:10 +0000

    test: check for core resources

--- fargate-eventbridge-cdk/cdk/test/cdk.test.ts
+++ fargate-eventbridge-cdk/cdk/test/cdk.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Cdk from '../lib/cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Validate stack resources', () => {
+  const app = new cdk.App();
+  const stack = new Cdk.CdkStack(app, 'MyTestStack');
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Events::EventBus', 1);
+  template.resourceCountIs('AWS::EC2::VPCEndpoint', 1);
+  template.resourceCountIs('AWS::ECS::Cluster', 1);
+  template.resourceCountIs('AWS::ECS::TaskDefinition', 1);
+  template.resourceCountIs('AWS::ECS::Service', 1);
+  template.resourceCountIs('AWS::ElasticLoadBalancingV2::LoadBalancer', 1);
 });
