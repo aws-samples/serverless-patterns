f26cdc40389009ed925f3b7f089b6cb35405b4c9
GlennChia <glennchia7@gmail.com>
2022-01-03 01:56:16 +0000

    feat: add basic event bus

--- fargate-eventbridge-cdk/cdk/lib/cdk-stack.ts
+++ fargate-eventbridge-cdk/cdk/lib/cdk-stack.ts
@@ -1,16 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { EventBus } from 'aws-cdk-lib/aws-events';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bus = new EventBus(this, 'bus', {
+      eventBusName: 'DemoEventBus'
+    });
   }
 }
