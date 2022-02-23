eb33daa3c0cfded9309c611049e4b8620042f85b
Edward Schaefer <ejschaefer@gmail.com>
2022-02-23 17:47:06 -0500

    migrated to AWS CDK v2 (python)

--- sfn-comprehend-sdk/app.py
+++ sfn-comprehend-sdk/app.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-import os
 
-from aws_cdk import core as cdk
 
-# For consistency with TypeScript code, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import App
 
 from sfn_comprehend_sdk.sfn_comprehend_sdk_stack import SfnComprehendSdkStack
 
 
-app = core.App()
+app = App()
 SfnComprehendSdkStack(app, "SfnComprehendSdkStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
