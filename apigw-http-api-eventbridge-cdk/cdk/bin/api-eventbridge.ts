f79e433311074d54504b6cc0c958584d180c3ebe
Edward Schaefer <ejschaefer@gmail.com>
2022-02-28 16:50:28 -0500

    migrated to AWS CDK v2 (typescript)

--- apigw-http-api-eventbridge-cdk/cdk/bin/api-eventbridge.ts
+++ apigw-http-api-eventbridge-cdk/cdk/bin/api-eventbridge.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
 import { ApiEventbridgeStack } from '../lib/api-eventbridge-stack';
 
-const app = new cdk.App();
+const app = new App();
 new ApiEventbridgeStack(app, 'ApiEventbridgeStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
