2768e9d3bf598102ff15d98e64863a5da17a353d
Edward Schaefer <ejschaefer@gmail.com>
2022-03-01 17:57:02 -0500

    migrated to AWS CDK v2 (typescript)

--- appsync-express-workflow/cdk/bin/appsync-express-workflow.ts
+++ appsync-express-workflow/cdk/bin/appsync-express-workflow.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { AppsyncExpressWorkflowStack } from '../lib/appsync-express-workflow-stack';
 
 const app = new cdk.App();
