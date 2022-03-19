810a970939a521d7744ebb68eb4ac042ef15e7ed
bbideep <bbideep@gmail.com>
2022-03-18 23:58:15 -0500

    Initial commit for the apigw-vpclink-pvt-alb terraform pattern

---
+++ apigw-vpclink-pvt-alb-terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  type = string
+  default = "vpc-123a45b6"
+}
+variable "private_subnets" {
+  type = list(any)
+  default = [
+      "subnet-5432bca1",
+      "subnet-1abc2345"
+  ]
+}
