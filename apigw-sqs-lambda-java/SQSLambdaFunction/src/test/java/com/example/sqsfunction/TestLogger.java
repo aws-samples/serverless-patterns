8d3489fa8f0e1f6388763a4851fffff9ff1c578e
Steven Cook <awscook@amazon.com>
2022-03-28 19:42:11 +1000

    Removed external deps from test code.

--- apigw-sqs-lambda-java/SQSLambdaFunction/src/test/java/com/example/sqsfunction/TestLogger.java
+++ apigw-sqs-lambda-java/SQSLambdaFunction/src/test/java/com/example/sqsfunction/TestLogger.java
@@ -1,19 +1,16 @@
 package com.example.sqsfunction;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class TestLogger implements LambdaLogger {
-    private static final Logger logger = LoggerFactory.getLogger(TestLogger.class);
 
     @Override
     public void log(String s) {
-        logger.info(s);
+        System.out.println(s);
     }
 
     @Override
     public void log(byte[] bytes) {
-        logger.info(new String(bytes));
+        System.out.println(new String(bytes));
     }
 }
