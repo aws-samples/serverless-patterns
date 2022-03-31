0c39cdf938b481b84211290582b7e3cd13310544
Steven Cook <awscook@amazon.com>
2022-03-31 20:56:21 +1000

    Using lambda events.

--- eventbridge-scheduled-lambda-java/EBLambdaFunction/src/main/java/com/example/App.java
+++ eventbridge-scheduled-lambda-java/EBLambdaFunction/src/main/java/com/example/App.java
@@ -2,34 +2,14 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.google.gson.GsonBuilder;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 
-import java.io.*;
-import java.util.HashMap;
 
-public class App implements RequestStreamHandler {
+public class App {
 
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-    @Override
-    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+    public void handleRequest(final ScheduledEvent event, final Context context) {
         LambdaLogger logger = context.getLogger();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-        try {
-            HashMap event = gson.fromJson(reader, HashMap.class);
-            logger.log(gson.toJson(event));
-            logger.log(String.format("Received event (%s) from source (%s)",
-                    event.get("detail-type"),
-                    event.get("source")));
-        } catch (JsonSyntaxException e) {
-            logger.log(e.toString());
-        } finally {
-            reader.close();
-        }
-
+        logger.log(String.format("Received event (%s) from source (%s)", event.getDetailType(), event.getSource()));
     }
 }
