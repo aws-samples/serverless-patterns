b5d0f5d325eb37cbd5f0a19e0ec577b6fccd3647
Hicham Terkiba <hterkiba@amazon.fr>
2022-03-31 11:16:20 +0200

    Updating license

--- fifo-sns-sqs-lambda-firehose-s3/assets/lambda/lambdaFirehoseLogger.py
+++ fifo-sns-sqs-lambda-firehose-s3/assets/lambda/lambdaFirehoseLogger.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
