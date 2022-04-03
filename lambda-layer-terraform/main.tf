93e1dcf0ccd5e0a5dce7e42086bb0e3f48df9910
GlennChia <glennchia7@gmail.com>
2022-04-03 14:39:00 +0000

    feat(lambda-layer): lambda layer mysql-connector

--- lambda-layer-terraform/main.tf
+++ lambda-layer-terraform/main.tf
@@ -33,3 +33,20 @@ resource "aws_iam_role" "this" {
 }
 EOF
 }
+
+data "archive_file" "layer_zip" {
+  type        = "zip"
+  source_dir  = "./dependencies/mysql-connector-python"
+  output_path = "./dependencies/zipped/mysql-connector-python.zip"
+}
+
+resource "aws_lambda_layer_version" "this" {
+  filename            = data.archive_file.layer_zip.output_path
+  layer_name          = "mysql-connector-python"
+  compatible_runtimes = ["python3.8"]
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256(data.archive_file.layer_zip.output_path)
+}
