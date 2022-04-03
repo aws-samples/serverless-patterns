6181105be20d812be157dead1ff28f0e78ae21d9
GlennChia <glennchia7@gmail.com>
2022-04-03 14:38:21 +0000

    feat(lambda-layer): add general iam lambda role

--- lambda-layer-terraform/main.tf
+++ lambda-layer-terraform/main.tf
@@ -12,3 +12,24 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_iam_role" "this" {
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
