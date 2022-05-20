60e2de16495c8dff5c5526e260ed24ecf95aabda
abblaya+1-at-277194667087 <abblaya@amazon.com>
2022-05-21 09:04:59 +1000

    Adding pattern apigw-method-cache

---
+++ apigw-method-cache/src/app.js
@@ -0,0 +1,13 @@
+/*! Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: MIT-0
+ */
+
+'use strict'
+
+exports.handler = async (event, context) => ({
+    "statusCode": 200,
+    "body": JSON.stringify(event),
+    "headers": {
+        "timestamp": +new Date
+    }
+});
