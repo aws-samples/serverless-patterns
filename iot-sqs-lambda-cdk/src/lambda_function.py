836ead3f9f85513494adb0705b5449fc3f9cc573
Patrick Malatesta <malatep@amazon.com>
2022-06-15 10:46:25 +0100

    new pattern iot-sqs-lambda-cdk

---
+++ iot-sqs-lambda-cdk/src/lambda_function.py
@@ -0,0 +1,15 @@
+import json
+
+def handler(event, context):
+    
+    #print the event object received by Lambda
+    print(json.dumps(event))
+    
+    # extract and log the body of the message received 
+    message_body = event["Records"][0]["body"]
+    print(message_body)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
