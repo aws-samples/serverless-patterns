877acc82abcf9882a46ff41303e706c647e4fc01
Corneliu Croitoru <ccroito@amazon.fr>
2022-06-18 13:48:48 +0200

    create stack

---
+++ elemental-medialive-mediapackage-cdk-ts/helpers/stream_config.ts
@@ -0,0 +1,49 @@
+import { aws_medialive as medialive } from "aws-cdk-lib";
+
+export type IMediaConnectConfig = {
+  whitelistCidr: string;
+  availabilityZone: {
+    a: string;
+    b: string;
+  };
+};
+
+export type IMediaLiveConfig = {
+
+  stream_name: string;
+  inputSecurityGroup: string;
+  inputSpecification: {
+    codec: string;
+    maximumBitrate: string;
+    resolution: string;
+  };
+  audioDescriptions: medialive.CfnChannel.AudioDescriptionProperty[];
+  videoDescriptions: medialive.CfnChannel.VideoDescriptionProperty[];
+  outputGroups: medialive.CfnChannel.OutputGroupProperty[];
+}
+
+
+export type IMediaPackageConfig = {
+
+  hls: boolean;
+  dash: boolean;
+  mss: boolean;
+  id_channel: string;
+  ip_sg_input: string;
+  stream_name: string;
+  hls_segment_duration_seconds: number;
+  hls_playlist_window_seconds: number;
+  hls_max_video_bits_per_second: number;
+  hls_min_video_bits_per_second: number;
+  hls_stream_order: string;
+  dash_segment_duration_seconds: number;
+  dash_manifest_window_seconds: number;
+  dash_max_video_bits_per_second: number;
+  dash_min_video_bits_per_second: number;
+  dash_stream_order: string;
+  mss_segment_duration_seconds: number;
+  mss_manifest_window_seconds: number;
+  mss_max_video_bits_per_second: number;
+  mss_min_video_bits_per_second: number;
+  mss_stream_order: string;
+}
