1b6c1179c100ad93f07bdb3759c76fe1ff42bcef
Corneliu Croitoru <ccroito@amazon.fr>
2022-06-19 16:00:55 +0200

    add ignored file

---
+++ elemental-mediaconnect-medialive-mediapackage-cdk-ts/lib/media_connect.ts
@@ -0,0 +1,72 @@
+import { Aws, aws_mediaconnect as mediaconnect, CfnOutput } from "aws-cdk-lib";
+
+import { Construct } from "constructs";
+import { loadMediaConnectConfig } from "../helpers/configuration";
+
+export class MediaConnect extends Construct {
+
+  public readonly flowArnA: string;
+  public readonly flowArnB: string;
+
+  private readonly protocol = "srt-listener";
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const config = loadMediaConnectConfig();
+
+    const cfnFlowA = new mediaconnect.CfnFlow(this, "MyCfnFlowA", {
+      name: Aws.STACK_NAME + "_" + "FlowA",
+      source: {
+        description: Aws.STACK_NAME + "_" + "FlowA",
+        ingestPort: config.ingestPort,
+        minLatency: config.minLatency,
+        name: Aws.STACK_NAME + "_" + "FlowA",
+        protocol: this.protocol,
+        whitelistCidr: config.whitelistCidr,
+      },
+      availabilityZone: config.availabilityZone.a,
+    });
+
+
+    const cfnFlowB = new mediaconnect.CfnFlow(this, "MyCfnFlowB", {
+      name: Aws.STACK_NAME + "_" + "FlowB",
+      source: {
+        description: Aws.STACK_NAME + "_" + "FlowB",
+        ingestPort: config.ingestPort,
+        minLatency: config.minLatency,
+        name: Aws.STACK_NAME + "_" + "FlowB",
+        protocol: this.protocol,
+        whitelistCidr: config.whitelistCidr,
+      },
+      availabilityZone: config.availabilityZone.b,
+    });
+
+    this.flowArnA = cfnFlowA.attrFlowArn;
+    this.flowArnB = cfnFlowB.attrFlowArn;
+
+    new CfnOutput(this, "MediaConnectFlowA", {
+      exportName: Aws.STACK_NAME + "-FLOW-A-SRT",
+      value:
+        cfnFlowA.attrSourceIngestIp + ":" + cfnFlowA.attrSourceSourceIngestPort,
+    });
+
+    new CfnOutput(this, "MediaConnectFlowB", {
+      exportName: Aws.STACK_NAME + "-FLOW-B-SRT",
+      value:
+        cfnFlowB.attrSourceIngestIp + ":" + cfnFlowB.attrSourceSourceIngestPort,
+    });
+
+
+    new CfnOutput(this, "flowAArn", {
+      exportName: Aws.STACK_NAME + "-FLOW-A-ARN",
+      value: cfnFlowA.attrFlowArn,
+    });
+
+    new CfnOutput(this, "flowBArn", {
+      exportName: Aws.STACK_NAME + "-FLOW-B-ARN",
+      value: cfnFlowB.attrFlowArn,
+    });
+
+  }
+}
