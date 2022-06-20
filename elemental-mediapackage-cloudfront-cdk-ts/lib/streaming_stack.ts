1eb44427b888de1c20d083106e12e9a0fbfa0929
Corneliu Croitoru <ccroito@amazon.fr>
2022-06-20 11:38:00 +0200

    add mediapackage & cloudfront

---
+++ elemental-mediapackage-cloudfront-cdk-ts/lib/streaming_stack.ts
@@ -0,0 +1,22 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CloudFront } from './cloudfront';
+import { MediaPackage } from './media_package';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class StreamingStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    //1. Create MediaPackage Channel
+    const mediaPackageChannel = new MediaPackage(
+      this,
+      "MediaPackage"
+    );
+
+    //2. Create CloudFront distribution
+    new CloudFront(this, "MyCloudFront", mediaPackageChannel.hlsEndpoint, mediaPackageChannel.dashEndpoint, mediaPackageChannel.mssEndpoint);
+
+  }
+
+}
