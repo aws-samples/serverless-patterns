9fa980c29a7994fc19332cd9e141eb8bd68516ca
Ashu Joshi <ashutjos@amazon.com>
2022-08-15 10:20:18 -0700

    Initial commit

---
+++ iot-dynamodbv2-ttl-cdk/iot-ddbv2/test/iot-ddbv2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IotDdbv2 from '../lib/iot-ddbv2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iot-ddbv2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IotDdbv2.IotDdbv2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
