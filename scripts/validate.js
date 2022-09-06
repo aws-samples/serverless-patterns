b6ed19ccab709c62c400270da0d27f8b9ad899ad
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2022-09-06 17:57:36 +0100

    testing

--- scripts/validate.js
+++ scripts/validate.js
@@ -54,34 +54,34 @@ const main = async () => {
       const errorList = errors.map((error, index) => `${index + 1}. \`${error.path}\`: ${error.stack}\n\n`);
 
       // Write comment back with errors for
-      await octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number: process.env.PR_NUMBER,
-        body:
-          `@${process.env.GITHUB_ACTOR} your 'example-pattern.json' is missing some key fields, please review below and address any errors you have \n\n` +
-          `${errorList.toString().replace(/,/g, '')} \n\n` +
-          `_If you need any help, take a look at the [example-pattern file](https://github.com/aws-samples/serverless-patterns/blob/main/_pattern-model/example-pattern.json)._ \n\n` +
-          `Make the changes, and push your changes back to this pull request. When all automated checks are successful, the Serverless DA team will process your pull request. \n\n`,
-      });
+      // await octokit.rest.issues.createComment({
+      //   owner,
+      //   repo,
+      //   issue_number: process.env.PR_NUMBER,
+      //   body:
+      //     `@${process.env.GITHUB_ACTOR} your 'example-pattern.json' is missing some key fields, please review below and address any errors you have \n\n` +
+      //     `${errorList.toString().replace(/,/g, '')} \n\n` +
+      //     `_If you need any help, take a look at the [example-pattern file](https://github.com/aws-samples/serverless-patterns/blob/main/_pattern-model/example-pattern.json)._ \n\n` +
+      //     `Make the changes, and push your changes back to this pull request. When all automated checks are successful, the Serverless DA team will process your pull request. \n\n`,
+      // });
 
-      await octokit.rest.issues.addLabels({
-        owner,
-        repo,
-        issue_number: process.env.PR_NUMBER,
-        labels: ['changes-required'],
-      });
+      // await octokit.rest.issues.addLabels({
+      //   owner,
+      //   repo,
+      //   issue_number: process.env.PR_NUMBER,
+      //   labels: ['changes-required'],
+      // });
 
       throw new Error('Failed to validate pattern, errors found');
     }
 
-    // Everything OK, remove this label
-    await octokit.rest.issues.removeLabel({
-      owner,
-      repo,
-      issue_number: process.env.PR_NUMBER,
-      name: 'changes-required',
-    });
+    // // Everything OK, remove this label
+    // await octokit.rest.issues.removeLabel({
+    //   owner,
+    //   repo,
+    //   issue_number: process.env.PR_NUMBER,
+    //   name: 'changes-required',
+    // });
 
     // Everything OK....
   } catch (error) {
