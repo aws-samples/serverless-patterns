af4f06387b943acaaa2fbc3666817f79f03f99f9
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2022-09-06 10:45:30 +0100

    testing

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,2 +1,10 @@
-console.log('VALIDATE 2');
+console.log('VALIDATE 3');
 console.log(process.env);
+
+const addedFiles = process.env.ADDED_FILES ? process.env.ADDED_FILES.split(',') : [];
+const modifiedFiles = process.env.MODIFIED_FILES ? process.env.MODIFIED_FILES.split(',') : [];
+
+console.log('addedFiles', addedFiles);
+console.log('modifiedFiles', modifiedFiles);
+
+// Get the pattern file....
