92a37be6f368f8bdcce77cb07c746da7f0af6b46
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2022-09-06 15:38:29 +0100

    testing

--- scripts/validate.js
+++ scripts/validate.js
@@ -128,7 +128,7 @@ const main = async () => {
         issue_number: process.env.PR_NUMBER,
         body:
           `@${process.env.GITHUB_ACTOR} your 'example-pattern.json' is missing some key fields, please review below and address any errors you have \n\n` +
-          `${errorList.toString()} \n\n` +
+          `${errorList.toString().replace(',', '')} \n\n` +
           `_If you need any help, take a look at the [example-pattern file](https://github.com/aws-samples/serverless-patterns/blob/main/_pattern-model/example-pattern.json)._ \n\n` +
           `Make the changes, and push your changes back to this pull request. When all automated checks are succesfull, the Serverless DA team will process your pull request. \n\n`,
       });
