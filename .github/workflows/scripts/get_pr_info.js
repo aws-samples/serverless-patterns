7d98692dd10d0c640e8fb0fd91172eecf05254ea
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2022-09-07 07:02:43 +0100

    testing

---
+++ .github/workflows/scripts/get_pr_info.js
@@ -0,0 +1,25 @@
+module.exports = async ({ github, context, core }) => {
+  const prNumber = process.env.PR_NUMBER;
+
+  if (prNumber === '') {
+    core.setFailed(`No PR number was passed. Aborting`);
+  }
+
+  try {
+    const {
+      data: { head, base },
+    } = await github.rest.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: prNumber,
+    });
+
+    core.setOutput('headRef', head.ref);
+    core.setOutput('headSHA', head.sha);
+    core.setOutput('baseRef', base.ref);
+    core.setOutput('baseSHA', base.sha);
+  } catch (error) {
+    core.setFailed(`Unable to retrieve info from PR number ${prNumber}.\n\n Error details: ${error}`);
+    throw error;
+  }
+};
