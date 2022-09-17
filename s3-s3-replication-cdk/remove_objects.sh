b6d62db8ff9157c41d8d1bfb175cbcdc7bcdfe90
Pubudu Jayawardana <pubudusj@gmail.com>
2022-09-17 23:17:01 +0200

    minor update to object remove script

--- s3-s3-replication-cdk/remove_objects.sh
+++ s3-s3-replication-cdk/remove_objects.sh
@@ -5,6 +5,6 @@ buckets="my-source-bucket my-destination-bucket-1 my-destination-bucket-2"
 for bucket_name in $buckets
 do
     echo ${bucket_name}
-    aws s3api delete-objects --bucket ${bucket_name} --delete "$(aws s3api list-object-versions --bucket ${bucket_name} --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}' --profile=personal)" > output.log
-    aws s3api delete-objects --bucket ${bucket_name} --delete "$(aws s3api list-object-versions --bucket ${bucket_name} --output=json --query='{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}' --profile=personal)" > output.log
+    aws s3api delete-objects --bucket ${bucket_name} --delete "$(aws s3api list-object-versions --bucket ${bucket_name} --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')" > output.log
+    aws s3api delete-objects --bucket ${bucket_name} --delete "$(aws s3api list-object-versions --bucket ${bucket_name} --output=json --query='{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}')" > output.log
 done
