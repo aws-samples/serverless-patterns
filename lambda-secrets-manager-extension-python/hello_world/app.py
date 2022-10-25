e2d9dd8af11637f039a7dcfc771a2afe6f414db1
Tim Pugh <nwtpugh@gmail.com>
2022-10-25 12:19:57 -0700

    Updated response with secret value (for testing only!)

--- lambda-secrets-manager-extension-python/hello_world/app.py
+++ lambda-secrets-manager-extension-python/hello_world/app.py
@@ -1,27 +1,32 @@
 import json
 import os
-import requests #We use the requests module to make a HTTP GET request to the Secrets Manager Lambda extension: https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_lambda.html
+import requests  # We use the requests module to make a HTTP GET request to the Secrets Manager Lambda extension: https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_lambda.html
 
 secret_name = os.environ.get('SECRET_NAME')
-secrets_extension_http_port = os.environ.get('PARAMETERS_SECRETS_EXTENSION_HTTP_PORT')
+secrets_extension_http_port = os.environ.get(
+    'PARAMETERS_SECRETS_EXTENSION_HTTP_PORT')
 aws_session_token = os.environ.get('AWS_SESSION_TOKEN')
 
-secrets_extension_endpoint = "http://localhost:" + secrets_extension_http_port + "/secretsmanager/get?secretId=" + secret_name
+secrets_extension_endpoint = "http://localhost:" + \
+    secrets_extension_http_port + "/secretsmanager/get?secretId=" + secret_name
 headers = {'X-Aws-Parameters-Secrets-Token': f'{aws_session_token}'}
+
 
 def lambda_handler(event, context):
 
     r = requests.get(secrets_extension_endpoint, headers=headers)
 
-    secret = json.loads(r.text)["SecretString"] #We use json.loads to parse our secret and account for special characters that may be in our secret and decode them.
+    # We use json.loads to parse our secret and account for special characters that may be in our secret and decode them.
+    secret = json.loads(r.text)["SecretString"]
 
-    print(secret) #We print our secret for demonstration purposes. Remove this line in your production systems!
+    # We print our secret for demonstration purposes. Remove this line in your production systems!
+    print(secret)
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world",
+                "secret": secret,
             }
         ),
     }
