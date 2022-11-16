b982561d82ed0a59d38f866e90faae361939b80f
maya flores <flormaya@amazon.com>
2022-11-16 14:33:28 -0800

    New pattern firehose transformation sam

---
+++ eventbridge-sqs-cdk/firehose-transformation-sam/src/index.js
@@ -0,0 +1,13 @@
+console.log('Loading function');
+
+exports.handler = async (event, context) => {
+    /* Process the list of records and transform them */
+    const output = event.records.map((record) => ({
+        /* This transformation is the "identity" transformation, the data is left intact */
+        recordId: record.recordId,
+        result: 'Ok',
+        data: record.data,
+    }));
+    console.log(`Processing completed.  Successful records ${output.length}.`);
+    return { records: output };
+};
