44ff43df4e927f3a52ddb2f21edae3c153953f90
Mark Sailes <msailes@amazon.co.uk>
2022-11-29 09:48:27 -0800

    Lambda SnapStart pattern

---
+++ apigw-lambda-snapstart/UnicornFunction/src/main/java/com/unicorn/store/StoreApplication.java
@@ -0,0 +1,23 @@
+package com.unicorn.store;
+
+import org.springframework.boot.SpringApplication;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return objectMapper;
+	}
+
+}
