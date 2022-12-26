b4a05ea28214be6bf713eee02647474c8001c7f6
Daniele Frasca <daniele.frasca@seven.one>
2022-12-26 09:55:07 +0100

    upgrade appconfig-lambda-rust

--- appconfig-lambda-rust/src/bin/handler.rs
+++ appconfig-lambda-rust/src/bin/handler.rs
@@ -1,4 +1,4 @@
-use lambda_runtime::{service_fn, Error, LambdaEvent};
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
@@ -16,25 +16,28 @@ async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
         .with_ansi(false)
         .without_time()
+        .with_max_level(tracing_subscriber::filter::LevelFilter::INFO)
         .init();
 
-    lambda_runtime::run(service_fn(|event: LambdaEvent<Value>| {
-        execute(event)
+    run(service_fn(|event: LambdaEvent<Value>| {
+        function_handler(event)
     }))
-    .await?;
-
-    Ok(())
+    .await
 }
 
-pub async fn execute(_event: LambdaEvent<Value>) -> Result<(), Error> {
+pub async fn function_handler(_event: LambdaEvent<Value>) -> Result<(), Error> {
     // ASSUME that this profile is coming from some logic and not from the env variable
-    let appconfig_profile = std::env::var("AWS_APPCONFIG_PROFILE").expect("AWS_APPCONFIG_PROFILE must be set");
+    let appconfig_profile =
+        std::env::var("AWS_APPCONFIG_PROFILE").expect("AWS_APPCONFIG_PROFILE must be set");
     if CONFIG.lock().unwrap().contains_key(&appconfig_profile) {
-      println!("CONFIG FROM HASHMAP{:?}", CONFIG.lock().unwrap().get(&appconfig_profile).unwrap());
+        println!(
+            "CONFIG FROM HASHMAP{:?}",
+            CONFIG.lock().unwrap().get(&appconfig_profile).unwrap()
+        );
     } else {
-      let config = fetch_config(&appconfig_profile).await?;
-      println!("ADDED INTO HASHMAP{:?}", appconfig_profile);
-      CONFIG.lock().unwrap().insert(appconfig_profile, config);
+        let config = fetch_config(&appconfig_profile).await?;
+        println!("ADDED INTO HASHMAP{:?}", appconfig_profile);
+        CONFIG.lock().unwrap().insert(appconfig_profile, config);
     }
 
     Ok(())
@@ -42,14 +45,16 @@ pub async fn execute(_event: LambdaEvent<Value>) -> Result<(), Error> {
 
 async fn fetch_config(appconfig_profile: &str) -> Result<MyConfig, Error> {
     let appconfig_name = std::env::var("APP_CONFIG_NAME").expect("APP_CONFIG_NAME must be set");
-    let appconfig_env = std::env::var("AWS_APPCONFIG_ENVIRONMENT").expect("AWS_APPCONFIG_ENVIRONMENT must be set");
-    let appconfig_port = std::env::var("AWS_APPCONFIG_EXTENSION_HTTP_PORT").expect("AWS_APPCONFIG_EXTENSION_HTTP_PORT must be set");
+    let appconfig_env =
+        std::env::var("AWS_APPCONFIG_ENVIRONMENT").expect("AWS_APPCONFIG_ENVIRONMENT must be set");
+    let appconfig_port = std::env::var("AWS_APPCONFIG_EXTENSION_HTTP_PORT")
+        .expect("AWS_APPCONFIG_EXTENSION_HTTP_PORT must be set");
 
-    let url = format!("http://localhost:{}/applications/{}/environments/{}/configurations/{}", appconfig_port, appconfig_name, appconfig_env, appconfig_profile);
-    let response = reqwest::get(url)
-        .await?
-        .json::<MyConfig>()
-        .await?;
+    let url = format!(
+        "http://localhost:{}/applications/{}/environments/{}/configurations/{}",
+        appconfig_port, appconfig_name, appconfig_env, appconfig_profile
+    );
+    let response = reqwest::get(url).await?.json::<MyConfig>().await?;
 
     Ok(response)
 }
