202a8851f4c4a0f630651f0465763e8d68717f63
Daniele Frasca <daniele.frasca@seven.one>
2022-12-26 10:23:32 +0100

    lambda-s3-rust

--- lambda-s3-rust/src/bin/handler.rs
+++ lambda-s3-rust/src/bin/handler.rs
@@ -1,21 +1,27 @@
-use aws_sdk_s3::ByteStream;
-use lambda_runtime::{handler_fn, Context, Error};
+use aws_sdk_s3::types::ByteStream;
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_ansi(false)
+        .without_time()
+        .with_max_level(tracing_subscriber::filter::LevelFilter::INFO)
+        .init();
     let config = aws_config::load_from_env().await;
     let s3_client = aws_sdk_s3::Client::new(&config);
 
-    lambda_runtime::run(handler_fn(|event: Value, ctx: Context| {
-        execute(&s3_client, event, ctx)
+    run(service_fn(|event: LambdaEvent<Value>| {
+        function_handler(&s3_client, event)
     }))
-    .await?;
-
-    Ok(())
+    .await
 }
 
-pub async fn execute(client: &aws_sdk_s3::Client, event: Value, _ctx: Context,) -> Result<(), Error> {
+pub async fn function_handler(
+    client: &aws_sdk_s3::Client,
+    event: LambdaEvent<Value>,
+) -> Result<(), Error> {
     println!("{:?}", event);
 
     let bucket = std::env::var("BUCKET_NAME").expect("BUCKET_NAME must be set");
