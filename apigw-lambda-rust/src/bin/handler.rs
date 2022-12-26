ab0839722e9d3d8a93a12f693930be203a5cbc6e
Daniele Frasca <daniele.frasca@seven.one>
2022-12-26 09:43:02 +0100

    upgrade apigw-lambda-rust

--- apigw-lambda-rust/src/bin/handler.rs
+++ apigw-lambda-rust/src/bin/handler.rs
@@ -1,33 +1,33 @@
-use lambda_http::{service_fn, Error, IntoResponse, Request, RequestExt, http::StatusCode, Response};
+use lambda_http::{
+    http::StatusCode, run, service_fn, Error, IntoResponse, Request, RequestExt, Response,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    lambda_http::run(service_fn(|event: Request| {
-        execute(event)
-    }))
-    .await?;
-    Ok(())
+    run(service_fn(|event: Request| function_handler(event))).await
 }
 
-pub async fn execute(event: Request) -> Result<impl IntoResponse, Error> {
+pub async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
     println!("EVENT {:?}", event);
 
     let body = event.payload::<MyPayload>()?;
 
     let response = Response::builder()
-            .status(StatusCode::OK)
-            .header("Content-Type", "application/json")
-            .body(json!({
-                "message": "Hello World",
-                "payload": body, 
-              }).to_string())
-            .unwrap();
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(
+            json!({
+              "message": "Hello World",
+              "payload": body,
+            })
+            .to_string(),
+        )
+        .unwrap();
 
     Ok(response)
 }
-
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct MyPayload {
