c64afd11fab82aecd32cf90f59b2cd8d92e6047a
Pubudu Jayawardana <pubudusj@gmail.com>
2023-01-13 10:57:08 +0100

    minor code cleanup

--- eventbridge-schedule-to-eventbridge-cdk-python/app.py
+++ eventbridge-schedule-to-eventbridge-cdk-python/app.py
@@ -7,22 +7,6 @@ from eventbridge_schedule_to_eventbridge_cdk_python.eventbridge_schedule_to_even
 
 
 app = cdk.App()
-EventbridgeScheduleToEventbridgeCdkPythonStack(app, "EventbridgeScheduleToEventbridgeCdkPythonStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+EventbridgeScheduleToEventbridgeCdkPythonStack(app, "EventbridgeScheduleToEventbridgeCdkPythonStack")
 
 app.synth()
