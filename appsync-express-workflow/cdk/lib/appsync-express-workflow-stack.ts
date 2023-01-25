1f9acd83f4337d47ddc318c77802c20643f73ea2
focusotter <mtliendo@focusotter.com>
2023-01-25 12:32:50 -0600

    update with JS func

--- appsync-express-workflow/cdk/lib/appsync-express-workflow-stack.ts
+++ appsync-express-workflow/cdk/lib/appsync-express-workflow-stack.ts
@@ -1,99 +1,109 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import {Pass, StateMachine, StateMachineType} from 'aws-cdk-lib/aws-stepfunctions';
-import { GraphqlApi, Schema, FieldLogLevel, AuthorizationType, MappingTemplate } from '@aws-cdk/aws-appsync-alpha';
-import { Role, ServicePrincipal, PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { join } from 'path';
-
-const START_EXECUTION_REQUEST_TEMPLATE = (stateMachineArn: String) => {
-  return `
-  {
-    "version": "2018-05-29",
-    "method": "POST",
-    "resourcePath": "/",
-    "params": {
-      "headers": {
-        "content-type": "application/x-amz-json-1.0",
-        "x-amz-target":"AWSStepFunctions.StartSyncExecution"
-      },
-      "body": {
-        "stateMachineArn": "${stateMachineArn}",
-        "name" : "$context.args.execution.name",
-        "input": "{ \\\"input\\\": \\\"$context.args.execution.input\\\"}"
-      }
-    }
-  }
-`
-}
-
-
-const RESPONSE_TEMPLATE = `
-## Raise a GraphQL field error in case of a datasource invocation error
-#if($ctx.error)
-  $util.error($ctx.error.message, $ctx.error.type)
-#end
-## if the response status code is not 200, then return an error. Else return the body **
-#if($ctx.result.statusCode == 200)
-    ## If response is 200, return the body.
-  $ctx.result.body
-#else
-    ## If response is not 200, append the response to error block.
-    $utils.appendError($ctx.result.body, $ctx.result.statusCode)
-#end
-`
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import {
+	Pass,
+	StateMachine,
+	StateMachineType,
+} from 'aws-cdk-lib/aws-stepfunctions'
+import {
+	GraphqlApi,
+	SchemaFile,
+	FieldLogLevel,
+	AppsyncFunction,
+	Code,
+	FunctionRuntime,
+	Resolver,
+} from 'aws-cdk-lib/aws-appsync'
+import { Role, ServicePrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam'
+import { join } from 'path'
 
 export class AppsyncExpressWorkflowStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props)
 
-    // The code that defines your stack goes here
-    const stateMachineDefinition = new Pass(this, "passState", {
-      result: { value: "Hello from StepFunctions!" }
-    });
+		// The code that defines your stack goes here
+		const stateMachineDefinition = new Pass(this, 'passState', {
+			result: { value: 'Hello from StepFunctions!' },
+		})
 
-    const stateMachine = new StateMachine(this, 'SyncStateMachine', {
-      definition: stateMachineDefinition,
-      stateMachineType: StateMachineType.EXPRESS
-    });
+		const stateMachine = new StateMachine(this, 'SyncStateMachine', {
+			definition: stateMachineDefinition,
+			stateMachineType: StateMachineType.EXPRESS,
+		})
 
-    const api = new GraphqlApi(this, 'Api', {
-      name: 'SyncStateMachineAPI',
-      schema: Schema.fromAsset(join(__dirname, 'schema.graphql')),
-      xrayEnabled: true,
-      logConfig: {
-        excludeVerboseContent: false,
-        fieldLogLevel: FieldLogLevel.ALL,
-      },
-    });
+		const api = new GraphqlApi(this, 'Api', {
+			name: 'SyncStateMachineAPI',
+			schema: SchemaFile.fromAsset(
+				join(__dirname, '../grpahql/schema.graphql')
+			),
+			xrayEnabled: true,
+			logConfig: {
+				excludeVerboseContent: false,
+				fieldLogLevel: FieldLogLevel.ALL,
+			},
+		})
 
-    const appsyncStepFunctionsRole = new Role(this, "SyncStateMachineRole", {
-      assumedBy: new ServicePrincipal("appsync.amazonaws.com")
-    });
-    appsyncStepFunctionsRole.addToPolicy(
-      new PolicyStatement({
-        resources: ["*"],
-        actions: ["states:StartSyncExecution"]
-      })
-    );
+		const appsyncStepFunctionsRole = new Role(this, 'SyncStateMachineRole', {
+			assumedBy: new ServicePrincipal('appsync.amazonaws.com'),
+		})
+		appsyncStepFunctionsRole.addToPolicy(
+			new PolicyStatement({
+				resources: ['*'],
+				actions: ['states:StartSyncExecution'],
+			})
+		)
 
-    const endpoint = "https://sync-states." + this.region + ".amazonaws.com/";
-    const httpdatasource = api.addHttpDataSource('StepFunctionsStateMachine', endpoint, {
-      authorizationConfig: { signingRegion: this.region, signingServiceName: 'states' },
-    });
+		const endpoint = 'https://sync-states.' + this.region + '.amazonaws.com/'
+		const httpdatasource = api.addHttpDataSource(
+			'StepFunctionsStateMachine',
+			endpoint,
+			{
+				authorizationConfig: {
+					signingRegion: this.region,
+					signingServiceName: 'states',
+				},
+			}
+		)
 
-    stateMachine.grant(httpdatasource.grantPrincipal, "states:StartSyncExecution");
+		stateMachine.grant(
+			httpdatasource.grantPrincipal,
+			'states:StartSyncExecution'
+		)
 
-    httpdatasource.createResolver({
-      typeName: 'Mutation',
-      fieldName: 'startExecution',
-      requestMappingTemplate: MappingTemplate.fromString(START_EXECUTION_REQUEST_TEMPLATE(stateMachine.stateMachineArn)),
-      responseMappingTemplate: MappingTemplate.fromString(RESPONSE_TEMPLATE)
-    })
+		const myJsFunction = new AppsyncFunction(this, 'function', {
+			name: 'my_js_function',
+			api,
+			dataSource: httpdatasource,
+			code: Code.fromAsset('directory/function_code.js'),
+			runtime: FunctionRuntime.JS_1_0_0,
+		})
 
+		const pipelineVars = JSON.stringify({
+			STATE_MACHINE_ARN: stateMachine.stateMachineArn,
+		})
+		new Resolver(this, 'PipelineResolver', {
+			api,
+			typeName: 'Mutation',
+			fieldName: 'startExecution',
+			code: Code.fromInline(`
+            // The before step
+            export function request(...args) {
+              console.log(args);
+              return ${pipelineVars}
+            }
+        
+            // The after step
+            export function response(ctx) {
+              return ctx.prev.result
+            }
+          `),
+			runtime: FunctionRuntime.JS_1_0_0,
+			pipelineConfig: [myJsFunction],
+		})
 
-    new CfnOutput(this, 'graphqlUrl', { value: api.graphqlUrl })
-    new CfnOutput(this, 'apiKey', { value: api.apiKey! })
-    new CfnOutput(this, 'apiId', { value: api.apiId })
-    new CfnOutput(this, 'stateMachine', {value: stateMachine.stateMachineArn})
-  }
+		new CfnOutput(this, 'graphqlUrl', { value: api.graphqlUrl })
+		new CfnOutput(this, 'apiKey', { value: api.apiKey! })
+		new CfnOutput(this, 'apiId', { value: api.apiId })
+		new CfnOutput(this, 'stateMachine', { value: stateMachine.stateMachineArn })
+	}
 }
