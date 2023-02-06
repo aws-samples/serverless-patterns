d99a6599e88b49e5b3702dee4df2196f9224ea9f
Tim Wondergem <timwondergem@gmail.com>
2023-02-05 22:52:18 -0500

    Creating a pattern to send DynamoDB stream records to SQS using EventBridge Pipes

---
+++ dynamo-pipes-sqs-cdk/src/DynamoPipeSqs/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+
+namespace DynamoPipeSqs;
+
+sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var app = new App();
+        new DynamoPipeSqsStack(app, "DynamoPipeSqsStack");
+
+        app.Synth();
+    }
+}
