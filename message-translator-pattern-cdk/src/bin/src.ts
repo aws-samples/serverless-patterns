ac075c7ec25cc705966ad31474a788b38c5d6940
Dominik Richter <doricht@amazon.de>
2023-02-07 11:55:47 +0100

    removed message-translator which was added here by accident

--- message-translator-pattern-cdk/src/bin/src.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { MessageTranslatorStack } from '../lib/message-translator-stack';
-
-const app = new cdk.App();
-new MessageTranslatorStack(app, 'MessageTranslatorStack');
