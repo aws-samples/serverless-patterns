ff14b452871397493139b6a35202abe0b2dfa053
Kimia Yahyazadeh <yahykimi@amazon.com>
2023-02-20 19:12:37 +0000

    added sns encyrption, sqs dlq, and queue enforce_ssl

--- cwlogs-lambda-sns-sqs-cdk/src/cwlogs_lambda_sns_sqs/cwlogs_lambda_sns_sqs_stack.py
+++ cwlogs-lambda-sns-sqs-cdk/src/cwlogs_lambda_sns_sqs/cwlogs_lambda_sns_sqs_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     Duration,
     Stack,
     CfnOutput,
+    aws_kms as kms,
     aws_lambda as _lambda,
     aws_logs as logs,
     aws_sns as sns,
@@ -18,15 +19,24 @@ class CwlogsLambdaSnsSqsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        #Create KMS Customer managed key for SNS server side encryption
+        key = kms.Key(self, "MyKey",
+            enable_key_rotation=True,
+            pending_window=Duration.days(7)
+        )
+        alias = key.add_alias("alias/custom")
+
         #Create an SNS topic
-        my_topic = sns.Topic(self, 'myLambdaTopic')
+        my_topic = sns.Topic(self, 'myLambdaTopic',
+            master_key=alias
+        )
         
         #Create a Lambda function
         my_lambda = _lambda.Function(self, "LogsLambdaFunction",
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset("src/lambda"),
             handler="index.lambda_handler",
-            timeout=Duration.seconds(30),
+            timeout=Duration.seconds(2),
             environment={'TOPIC_ARN': my_topic.topic_arn}
         )
 
@@ -54,6 +64,15 @@ class CwlogsLambdaSnsSqsStack(Stack):
             ],
             resources=[my_topic.topic_arn],
         ))
+
+        my_lambda.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=[
+                "kms:GenerateDataKey*",
+                "kms:Decrypt"
+            ],
+            resources=[key.key_arn],
+        ))
         
         #Add Lambda as a destination for log subscription
         my_log_group.add_subscription_filter(
@@ -61,14 +80,24 @@ class CwlogsLambdaSnsSqsStack(Stack):
             destination=log_destinations.LambdaDestination(fn=my_lambda),
             filter_pattern=logs.FilterPattern.all_terms("ERROR")
         )
-
+        #Create an SQS dead letter queue
+        my_dead_letter_queue = sqs.Queue(self, "myDeadLetterQueue",
+            enforce_ssl=True
+        )
         #Create an SQS queue
-        my_queue = sqs.Queue(self, "myQueue")
+        my_queue = sqs.Queue(self, "myQueue",
+            enforce_ssl=True,
+            dead_letter_queue = sqs.DeadLetterQueue(
+                max_receive_count=2,
+                queue=my_dead_letter_queue
+        ))
+
         
         #Subscribe SQS queue to SNS topic
-        my_topic.add_subscription(
-            sns_sub.SqsSubscription(my_queue, raw_message_delivery=True)
-        )
+        my_topic.add_subscription(sns_sub.SqsSubscription(
+            my_queue, 
+            raw_message_delivery=True
+        ))
 
         #Stack Outputs
         CfnOutput(self, "LOG GROUP NAME", 
