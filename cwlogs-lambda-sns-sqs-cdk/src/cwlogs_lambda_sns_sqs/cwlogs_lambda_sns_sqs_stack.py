9048b8b78126a480bf9e5dd0be004f00d3176015
Kimia Yahyazadeh <yahykimi@amazon.com>
2023-02-23 19:16:02 +0000

    removed references to "my"

--- cwlogs-lambda-sns-sqs-cdk/src/cwlogs_lambda_sns_sqs/cwlogs_lambda_sns_sqs_stack.py
+++ cwlogs-lambda-sns-sqs-cdk/src/cwlogs_lambda_sns_sqs/cwlogs_lambda_sns_sqs_stack.py
@@ -20,52 +20,52 @@ class CwlogsLambdaSnsSqsStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         #Create KMS Customer managed key for SNS server side encryption
-        key = kms.Key(self, "MyKey",
+        key = kms.Key(self, "Key",
             enable_key_rotation=True,
             pending_window=Duration.days(7)
         )
-        alias = key.add_alias("alias/custom")
+        alias = key.add_alias("alias/queuecustomkey")
 
         #Create an SNS topic
-        my_topic = sns.Topic(self, 'myLambdaTopic',
+        topic = sns.Topic(self, 'LambdaTopic',
             master_key=alias
         )
         
         #Create a Lambda function
-        my_lambda = _lambda.Function(self, "LogsLambdaFunction",
+        subscribed_lambda = _lambda.Function(self, "SubscribedFunction",
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset("src/lambda"),
             handler="index.lambda_handler",
             timeout=Duration.seconds(2),
-            environment={'TOPIC_ARN': my_topic.topic_arn}
+            environment={'TOPIC_ARN': topic.topic_arn}
         )
 
         #Create a CloudWatch Log group and stream
-        my_log_group = logs.LogGroup(self, "myLogGroup")
+        log_group = logs.LogGroup(self, "LogGroup")
         
-        my_log_stream = logs.LogStream(self, "myLogStream",
-            log_group=my_log_group,
+        log_stream = logs.LogStream(self, "LogStream",
+            log_group=log_group,
         )
 
         #IAM role to invoke Lambda 
-        my_lambda.add_to_role_policy(iam.PolicyStatement(
+        subscribed_lambda.add_to_role_policy(iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
             actions=[
                 "logs:PutLogEvents"
             ],
-            resources=[my_log_group.log_group_arn],
+            resources=[log_group.log_group_arn],
         ))
 
         #IAM Role to publish to SNS
-        my_lambda.add_to_role_policy(iam.PolicyStatement(
+        subscribed_lambda.add_to_role_policy(iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
             actions=[
                 "sns:Publish"
             ],
-            resources=[my_topic.topic_arn],
+            resources=[topic.topic_arn],
         ))
 
-        my_lambda.add_to_role_policy(iam.PolicyStatement(
+        subscribed_lambda.add_to_role_policy(iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
             actions=[
                 "kms:GenerateDataKey*",
@@ -75,39 +75,39 @@ class CwlogsLambdaSnsSqsStack(Stack):
         ))
         
         #Add Lambda as a destination for log subscription
-        my_log_group.add_subscription_filter(
+        log_group.add_subscription_filter(
             id="lambdaSubscription",
-            destination=log_destinations.LambdaDestination(fn=my_lambda),
+            destination=log_destinations.LambdaDestination(fn=subscribed_lambda),
             filter_pattern=logs.FilterPattern.all_terms("ERROR")
         )
         #Create an SQS dead letter queue
-        my_dead_letter_queue = sqs.Queue(self, "myDeadLetterQueue",
+        dead_letter_queue = sqs.Queue(self, "DeadLetterQueue",
             enforce_ssl=True
         )
         #Create an SQS queue
-        my_queue = sqs.Queue(self, "myQueue",
+        main_queue = sqs.Queue(self, "MainQueue",
             enforce_ssl=True,
             dead_letter_queue = sqs.DeadLetterQueue(
                 max_receive_count=2,
-                queue=my_dead_letter_queue
+                queue=dead_letter_queue
         ))
 
         
         #Subscribe SQS queue to SNS topic
-        my_topic.add_subscription(sns_sub.SqsSubscription(
-            my_queue, 
+        topic.add_subscription(sns_sub.SqsSubscription(
+            main_queue, 
             raw_message_delivery=True
         ))
 
         #Stack Outputs
         CfnOutput(self, "LOG GROUP NAME", 
-            value=my_log_group.log_group_name
+            value=log_group.log_group_name
             )
         CfnOutput(self, "LOG STREAM NAME", 
-            value=my_log_stream.log_stream_name
+            value=log_stream.log_stream_name
             )
         CfnOutput(self, "QUEUE URL", 
-            value=my_queue.queue_url
+            value=main_queue.queue_url
             )
 
         
