0c71e683d76831327e3d9c5a488c2d3ceb06390b
Dmitry Gulin <gulid@amazon.com>
2023-03-20 18:31:49 -0400

    add AWS CLI v1 compatibility for test.sh

--- lambda-esm-kinesis-filters-sam/test.sh
+++ lambda-esm-kinesis-filters-sam/test.sh
@@ -7,6 +7,9 @@
 
 command -v aws >/dev/null 2>&1 || { echo >&2 "This script requires AWS CLI (https://aws.amazon.com/cli/) to be installed.  Aborting."; exit 1; }
 
+# detect AWS CLI major version number
+aws_version=$(aws --version 2>&1 | cut -d '.' -f1 | cut -d '/' -f2)
+
 stream='stream-lambda-esm-filter'
 
 for f in ./events/*
@@ -15,10 +18,20 @@ do
     key="${fileName%.*}"
 
     echo "Sending $key payload..."
-    aws kinesis put-record \
-        --stream-name $stream \
-        --data $(base64 -i "$f") \
-        --partition-key $key
+    if [ $aws_version -eq 1 ]
+    then
+        # send payload as-is to AWS CLI v1
+        aws kinesis put-record \
+            --stream-name "$stream" \
+            --data  "$(cat "$f")" \
+            --partition-key "$key"
+    else
+        # AWS CLI v2 expects payload data to be base64 encoded
+        aws kinesis put-record \
+            --stream-name "$stream" \
+            --data  "$(base64 -i "$f")" \
+            --partition-key "$key"
+    fi
 
     echo '--------------------'
 done
