4760bda74e1f02c50aacd63fb5743ac860f3ba2f
Mohamad Soufan <ms584@evansville.edu>
2023-04-26 15:10:22 -0400

    used Arn.format() organized the files

--- eventbridge-pipes-ddb-stream-apigateway-iot-core-cdk/cdk/bin/cdk.ts
+++ eventbridge-pipes-ddb-stream-apigateway-iot-core-cdk/cdk/bin/cdk.ts
@@ -1,34 +1,24 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { EventBridgePipesIotCoreStack } from '../lib/cdk-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { EventBridgePipesIotCoreStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
 
-const IOT_EVENTS_TOPIC_NAME = 'iot-test-topic';
-const IOT_DATA_ENDPOINT = '{paste your endpoint here}'; // You must replace this with aws iot endpoint found in IotCore settings
-const DYNAMODB_TABLE_NAME = 'iot-events-table';
-const REST_API_NAME = 'iot-rest-api';
-const REST_API_RESOURCE = 'iot';
-const PIPE_NAME = 'iot-pipe';
+const IOT_EVENTS_TOPIC_NAME = "iot-test-topic";
+const IOT_DATA_ENDPOINT = "{paste your endpoint here}"; // You must replace this with aws iot endpoint found in IotCore settings
+const DYNAMODB_TABLE_NAME = "iot-events-table";
+const REST_API_NAME = "iot-rest-api";
+const REST_API_RESOURCE = "iot";
+const PIPE_NAME = "iot-pipe";
 
-new EventBridgePipesIotCoreStack(app, 'EventBridgePipesIotCoreStack', {
+new EventBridgePipesIotCoreStack(app, "EventBridgePipesIotCoreStack", {
   iotTopicName: IOT_EVENTS_TOPIC_NAME,
   iotDataEndpoint: IOT_DATA_ENDPOINT,
   dynamoDbTableName: DYNAMODB_TABLE_NAME,
   apiGateway: {
     restApiName: REST_API_NAME,
-    apiResource: REST_API_RESOURCE,
+    iotResourcePath: REST_API_RESOURCE,
   },
   pipeName: PIPE_NAME,
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- eventbridge-pipes-ddb-stream-apigateway-iot-core-cdk/cdk/lib/cdk-stack.ts
+++ eventbridge-pipes-ddb-stream-apigateway-iot-core-cdk/cdk/lib/cdk-stack.ts
@@ -1,11 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { RestApi, EndpointType, AwsIntegration } from 'aws-cdk-lib/aws-apigateway';
-import { AttributeType, BillingMode, StreamViewType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { EventBus } from 'aws-cdk-lib/aws-events';
-import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-import { StartingPosition } from 'aws-cdk-lib/aws-lambda';
-import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import {
+  RestApi,
+  EndpointType,
+  AwsIntegration,
+} from "aws-cdk-lib/aws-apigateway";
+import {
+  AttributeType,
+  BillingMode,
+  StreamViewType,
+  Table,
+} from "aws-cdk-lib/aws-dynamodb";
+import { EventBus } from "aws-cdk-lib/aws-events";
+import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { StartingPosition } from "aws-cdk-lib/aws-lambda";
+import { CfnPipe } from "aws-cdk-lib/aws-pipes";
+import { Construct } from "constructs";
 
 export interface EventBridgePipesIotCoreStackProps extends cdk.StackProps {
   iotTopicName: string;
@@ -13,56 +22,74 @@ export interface EventBridgePipesIotCoreStackProps extends cdk.StackProps {
   dynamoDbTableName: string;
   apiGateway: {
     restApiName: string;
-    apiResource: string;
+    iotResourcePath: string;
   };
   pipeName: string;
 }
 export class EventBridgePipesIotCoreStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: EventBridgePipesIotCoreStackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props?: EventBridgePipesIotCoreStackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    const eventsTable = new Table(this, 'Iot-Table', {
+    // Create Dynamo Table with Stream enabled
+    const eventsTable = new Table(this, "Iot-Table", {
       tableName: props?.dynamoDbTableName,
-      partitionKey: { name: 'pk', type: AttributeType.STRING },
-      sortKey: { name: 'sk', type: AttributeType.STRING },
+      partitionKey: { name: "pk", type: AttributeType.STRING },
+      sortKey: { name: "sk", type: AttributeType.STRING },
       billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       stream: StreamViewType.NEW_AND_OLD_IMAGES,
     });
 
-    // default event bus
-    const defaultEventBus = EventBus.fromEventBusName(this, 'Default-Event-Bus', 'default');
+    // Get account default event bus
+    const defaultEventBus = EventBus.fromEventBusName(
+      this,
+      "Default-Event-Bus",
+      "default"
+    );
 
-    const iotTopicApi = new RestApi(this, 'Iot-Rest-Api', {
+    // Create Rest API
+    const iotTopicApi = new RestApi(this, "Iot-Rest-Api", {
       restApiName: props?.apiGateway.restApiName,
       endpointTypes: [EndpointType.REGIONAL],
-      defaultCorsPreflightOptions: { allowOrigins: ['*'] },
+      defaultCorsPreflightOptions: { allowOrigins: ["*"] },
       deployOptions: {
-        stageName: 'dev',
+        stageName: "dev",
       },
     });
 
-    const iotTopicApiEndpoint = iotTopicApi.root.addResource(props?.apiGateway.apiResource!);
-    const iotTopicEndpointSubdomain = props?.iotDataEndpoint.split('.')[0];
+    // Add a REST resources with direct AWS Integration to IoT Core
+    const iotTopicApiEndpoint = iotTopicApi.root.addResource(
+      props?.apiGateway.iotResourcePath!
+    );
+    const iotTopicEndpointSubdomain = props?.iotDataEndpoint.split(".")[0];
     const iotServiceIntegration = new AwsIntegration({
-      service: 'iotdata',
+      service: "iotdata",
       proxy: false,
       subdomain: iotTopicEndpointSubdomain,
       path: `topics/${props?.iotTopicName}`,
-      integrationHttpMethod: 'POST',
+      integrationHttpMethod: "POST",
       options: {
-        credentialsRole: new Role(this, 'Iot-Topic-Endpoint-Role', {
-          assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+        credentialsRole: new Role(this, "Iot-Topic-Endpoint-Role", {
+          assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
           managedPolicies: [
-            { managedPolicyArn: 'arn:aws:iam::aws:policy/AWSIoTDataAccess' },
-            { managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs' },
+            { managedPolicyArn: "arn:aws:iam::aws:policy/AWSIoTDataAccess" },
+            {
+              managedPolicyArn:
+                "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs",
+            },
           ],
         }),
         integrationResponses: [
           {
-            statusCode: '200',
-            responseParameters: { 'method.response.header.Content-Type': 'integration.response.header.Content-Type' },
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Content-Type":
+                "integration.response.header.Content-Type",
+            },
           },
         ],
       },
@@ -70,29 +97,38 @@ export class EventBridgePipesIotCoreStack extends cdk.Stack {
 
     const methodResponses = [
       {
-        statusCode: '200',
+        statusCode: "200",
         responseParameters: {
-          'method.response.header.Content-Type': true,
+          "method.response.header.Content-Type": true,
         },
       },
     ];
-    iotTopicApiEndpoint.addMethod('POST', iotServiceIntegration, { methodResponses });
+    const apiMethod = "POST";
+    iotTopicApiEndpoint.addMethod(apiMethod, iotServiceIntegration, {
+      methodResponses,
+    });
 
-    const pipeRole = new Role(this, 'Events-Role', {
-      assumedBy: new ServicePrincipal('pipes.amazonaws.com'),
+    const apiStage = iotTopicApi.deploymentStage.stageName;
+    const apiId = iotTopicApi.restApiId;
+    const iotResourcePath = props?.apiGateway.iotResourcePath;
+    const apiResource = `${apiId}/${apiStage}/${apiMethod}/${iotResourcePath}`;
+
+    // Endpoint ARN to be targeted by the pipe
+    const targetIotTopicIntegrationEndpointArn = cdk.Arn.format(
+      { service: "execute-api", resource: apiResource },
+      cdk.Stack.of(this)
+    );
+
+    // Create new Pipe Role
+    const pipeRole = new Role(this, "Events-Role", {
+      assumedBy: new ServicePrincipal("pipes.amazonaws.com"),
     });
 
     eventsTable.grantStreamRead(pipeRole);
     defaultEventBus.grantPutEventsTo(pipeRole);
 
-    const { account, region } = cdk.Stack.of(this);
-    const apiStage = iotTopicApi.deploymentStage.stageName;
-    const apiId = iotTopicApi.restApiId;
-    const apiMethod = 'POST';
-    const apiPath = props?.apiGateway.apiResource;
-    const targetEndpointArn = `arn:aws:execute-api:${region}:${account}:${apiId}/${apiStage}/${apiMethod}/${apiPath}`;
     // Create new Pipe
-    const eventsPipe = new CfnPipe(this, 'Events-Pipe', {
+    const eventsPipe = new CfnPipe(this, "Events-Pipe", {
       name: props?.pipeName,
       roleArn: pipeRole.roleArn,
       //@ts-ignore
@@ -110,17 +146,17 @@ export class EventBridgePipesIotCoreStack extends cdk.Stack {
           ],
         },
       },
-      target: targetEndpointArn,
+      target: targetIotTopicIntegrationEndpointArn,
     });
 
-    new cdk.CfnOutput(this, 'IotDataEndpoint', {
+    // Stack outputs
+    new cdk.CfnOutput(this, "IotDataEndpoint", {
       value: props?.iotDataEndpoint!,
     });
-    new cdk.CfnOutput(this, 'IotTopicName', {
+    new cdk.CfnOutput(this, "IotTopicName", {
       value: props?.iotTopicName!,
     });
-
-    new cdk.CfnOutput(this, 'DynamoDBTableName', {
+    new cdk.CfnOutput(this, "DynamoDBTableName", {
       value: eventsTable.tableName,
     });
   }
