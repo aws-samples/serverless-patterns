a799b0e2b8253061a0aa6fc0df53c071aee97efd
Joel Junior <junior.tads@gmail.com>
2023-05-10 16:14:14 -0300

    added feature CDK for dotnet using apigw+lambda+qldb

---
+++ apigw-lambda-qldb-cdk-dotnet/src/lambda/dotnet-core-web-api/dotnet-core-web-api/Models/Person.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_core_web_api.Models;
+
+public class Person
+{   
+    public string Email { get; set; }
+
+    public string Name { get; set; }
+}
