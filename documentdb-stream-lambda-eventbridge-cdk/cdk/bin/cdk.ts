3ede3aa9b02196575f76d7c2930a5b0e795e189d
Mohamad Soufan <ms584@evansville.edu>
2023-07-04 19:44:54 -0400

    added db stack

--- documentdb-stream-lambda-eventbridge-cdk/cdk/bin/cdk.ts
+++ documentdb-stream-lambda-eventbridge-cdk/cdk/bin/cdk.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DocumentDbStreamLambdaEventBridgeStack } from '../lib/cdk-stack';
+import { DocumentDbStack } from '../lib/db-stack';
 
 const app = new cdk.App();
 
@@ -20,4 +21,8 @@ new DocumentDbStreamLambdaEventBridgeStack(app, 'DocumentDbStreamLambdaEventBrid
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+new DocumentDbStack(this, 'DocumentDbStack', {
+  secretName: 'DocumentDBSecret',
 });
---
+++ documentdb-stream-lambda-eventbridge-cdk/cdk/lib/db-stack.ts
@@ -0,0 +1,72 @@
+import { Construct } from 'constructs';
+
+import {
+  App,
+  Stack,
+  StackProps,
+  Duration,
+  CfnOutput,
+  RemovalPolicy,
+  aws_ec2 as ec2,
+  aws_rds as rds,
+  aws_iam as iam,
+  custom_resources as cr,
+  aws_lambda as lambda,
+  aws_lambda_nodejs as nodejs,
+  aws_events as events,
+  aws_docdb as docdb,
+  aws_events_targets as targets,
+  aws_secretsmanager as secrets,
+  SecretValue,
+} from 'aws-cdk-lib';
+
+const MONGO_DB_PORT = 27017;
+
+export interface DocumentDbStackProps extends StackProps {
+  secretName: string;
+}
+
+export class DocumentDbStack extends Stack {
+  constructor(scope: Construct, id: string, props?: DocumentDbStackProps) {
+    super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, 'default-vpc-id', {
+      isDefault: true,
+    });
+
+    const docDbSg = new ec2.SecurityGroup(this, 'DocDB-SG', {
+      vpc: vpc,
+    });
+
+    docDbSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(MONGO_DB_PORT));
+
+    const cluster = new docdb.DatabaseCluster(this, 'Database', {
+      masterUser: {
+        username: 'myuser', // NOTE: 'admin' is reserved by DocumentDB
+        secretName: 'DocumentDBSecret', // optional, if you prefer to specify the secret name
+      },
+      dbClusterName: 'DocDbCluster',
+      securityGroup: docDbSg,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
+      engineVersion: '4.0.0',
+      instances: 1,
+      vpc,
+    });
+
+    new CfnOutput(this, 'Docdb-endpoint-hostname', {
+      value: `${cluster.clusterEndpoint.hostname}`,
+    });
+    new CfnOutput(this, 'Docdb-endpoint-port', {
+      value: `${cluster.clusterEndpoint.port}`,
+    });
+    new CfnOutput(this, 'Docdb-cluster-identifier', {
+      value: `${cluster.clusterIdentifier}`,
+    });
+    new CfnOutput(this, 'Docdb-cluster-resource-identifier', {
+      value: `${cluster.clusterResourceIdentifier}`,
+    });
+    new CfnOutput(this, 'Docdb-cluster-secret-arn', {
+      value: `${cluster.secret?.secretArn}`,
+    });
+  }
+}
