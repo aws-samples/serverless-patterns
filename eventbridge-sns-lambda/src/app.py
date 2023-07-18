7009129bc759c3629096e82cc62d9a295f73012a
David Boyne <boyneyy123@gmail.com>
2023-07-18 09:31:15 +0100

    Update eventbridge-sns-lambda/src/app.py

--- eventbridge-sns-lambda/src/app.py
+++ eventbridge-sns-lambda/src/app.py
@@ -16,6 +16,5 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            # "location": ip.text.replace("\n", "")
         }),
     }
