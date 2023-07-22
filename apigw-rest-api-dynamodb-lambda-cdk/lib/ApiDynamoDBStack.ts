0acddba446599b67ff67638f6a326e979bee61d5
Thi <ngdinhthi@gmail.com>
2023-07-22 23:09:28 +0700

    clean up

--- apigw-rest-api-dynamodb-lambda-cdk/lib/ApiDynamoDBStack.ts
+++ apigw-rest-api-dynamodb-lambda-cdk/lib/ApiDynamoDBStack.ts
@@ -24,10 +24,10 @@ export class ApiDynamoDBStack extends Stack {
     });
     
     const lambdaFunction = new lambda.Function(this, 'Function', {
-      code: lambda.Code.fromAsset('./src'),
-      handler: 'index.handlerAAAA',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../src')),
+      handler: 'index.handler',
       functionName: 'TableStreamHandler',
-      runtime: lambda.Runtime.NODEJS_16_X,
+      runtime: lambda.Runtime.NODEJS_18_X,
     });
 
     lambdaFunction.addEventSource(new DynamoEventSource(dynamoDBTable, {
