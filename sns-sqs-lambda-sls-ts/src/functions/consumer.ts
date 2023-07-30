8deb0e0b15d3fb490fd837316e52a140703d064c
kyojin-indie <kyojinindie@gmail.com>
2023-07-30 10:06:00 -0600

    Revert "sns-sqs-lambda whit sls and ts example"
    This reverts commit db122d1d513bef04c019fbf8c49e4c92c594516d.

--- sns-sqs-lambda-sls-ts/src/functions/consumer.ts
+++
@@ -1,5 +0,0 @@
-import {SQSEvent, SQSHandler, Context} from "aws-lambda";
-
-export const handler: SQSHandler = async (event: SQSEvent, context: Context): Promise<void> => {
-    console.log({event, context})
-}
