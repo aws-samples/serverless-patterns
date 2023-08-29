e6fc888a17e90307cc005793244431eb3d7bf38d
Mia Alarcon Chong <miachong@amazon.com>
2023-08-29 19:04:33 -0400

    Changes/fixes to serverless pattern

---
+++ cloudtrail-lambda-dynamo-cdk/src/lib/lambda/object_tag_checker/objects_db.py
@@ -0,0 +1,28 @@
+from botocore.exceptions import ClientError
+
+class Objects:
+    def __init__(self, db_client, table_name):
+        self.dyn_client = db_client
+        self.table = self.dyn_client.Table(table_name)
+
+    def add_key(self, item, attributes):
+        update_expression = "SET {}".format(
+            ",".join(f"#{key}=:{key}" for key in attributes)
+        )
+        attribute_values = {f":{key}": value for key, value in attributes.items()}
+        attribute_names = {f"#{key}": key for key in attributes}
+
+        try:
+            response = self.table.update_item(
+                Key={
+                    "object_arn": item.object_arn,
+                },
+                UpdateExpression=update_expression,
+                ExpressionAttributeValues=attribute_values,
+                ExpressionAttributeNames=attribute_names,
+            )
+            return response
+
+        except ClientError as err:
+            print(err)
+            raise
