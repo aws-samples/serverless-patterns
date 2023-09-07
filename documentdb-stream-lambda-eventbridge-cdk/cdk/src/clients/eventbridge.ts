84e24d286bad3b826eac7560d802f2bef22d4e9b
Majd Soufan <ms585@evansville.edu>
2023-09-06 22:29:39 -0400

    Delete documentdb-stream-lambda-eventbridge-cdk directory

--- documentdb-stream-lambda-eventbridge-cdk/cdk/src/clients/eventbridge.ts
+++
@@ -1,5 +0,0 @@
-import { EventBridgeClient } from '@aws-sdk/client-eventbridge'
-
-export const eventBridgeClient = new EventBridgeClient({
-  region: process.env.AWS_REGION!
-})
--- documentdb-stream-lambda-eventbridge-cdk/cdk/src/lambdas/product-created.ts
+++
@@ -1,7 +0,0 @@
-import { EventBridgeEvent } from 'aws-lambda';
-
-const productCreated = async (event: EventBridgeEvent<string, string>): Promise<void> => {
-  console.log('Product Created with Event: ', event);
-};
-
-export const main = productCreated;
