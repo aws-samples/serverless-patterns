fa95f087576a1a30dc1a85d2cd6bab07038a2aa0
Max Berta <masberta@amazon.com>
2023-10-12 16:34:08 +0100

    updated to 3.11 runtime

--- lambda-bedrock-cdk/lambda-layer/lib/lambda-layer-stack.ts
+++ lambda-bedrock-cdk/lambda-layer/lib/lambda-layer-stack.ts
@@ -10,12 +10,12 @@ export class LambdaLayerStack extends cdk.Stack {
     const layer = new lambda.LayerVersion(this, 'HelperLayer', {
       code: lambda.Code.fromAsset('resources/layers/bedrock-layer'),
       description: 'Boto3 with bedrock support',
-      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_11],
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
     const fn = new lambda.Function(this, 'LambdaFunction', {
-        runtime: lambda.Runtime.PYTHON_3_9,
+        runtime: lambda.Runtime.PYTHON_3_11,
         code: lambda.Code.fromAsset('resources/lambda'),
         handler: 'lambda_function.handler',
         timeout: cdk.Duration.seconds(50),
