bb0a36177dd4b20e8c5b97d17c8aa31ffc195712
Cody Currie <cjcurrie@amazon.com>
2023-10-24 13:38:00 -0700

    fixed import

--- cdk-codepipeline-cross-account-deployment/aws_serverless_pattern_cdk_crossaccount_deployment/static_site/website_stage.py
+++ cdk-codepipeline-cross-account-deployment/aws_serverless_pattern_cdk_crossaccount_deployment/static_site/website_stage.py
@@ -1,6 +1,6 @@
 import aws_cdk as cdk
 from constructs import Construct
-from aws_serverless_pattern_cdk_crossaccount_deployment.static_site import DemoWebsiteStack
+from aws_serverless_pattern_cdk_crossaccount_deployment.static_site.website_stack import DemoWebsiteStack
 
 class WebsiteStage(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
