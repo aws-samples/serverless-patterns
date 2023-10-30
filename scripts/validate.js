22f94028f3fca7d4df900634bde0dec46a20636e
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2023-10-30 15:15:24 +0000

    adding new scripts for more pattern validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,19 +1,54 @@
 const fs = require('fs');
 const path = require('path');
 const { Octokit } = require('@octokit/rest');
+const fetch = require('node-fetch');
 
 const Validator = require('jsonschema').Validator;
 const v = new Validator();
 const schema = require('./pattern-schema.json');
+const { ValidationError } = require('jsonschema');
 
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-const githubAutomation = process.env.GH_AUTOMATION ? process.env.GH_AUTOMATION === 'true' : true;
+const githubAutomation = process.env.GH_AUTOMATION ? process.env.GH_AUTOMATION === 'true' : false;
 
 const octokit = new Octokit({
   auth: process.env.TOKEN,
 });
 
 console.info(process.env);
+
+// Returns an array of errors if any are found.
+const customValidate = async (patternFile) => {
+  const errors = [];
+
+  const { gitHub: { template: { repoURL, templateURL, projectFolder, templateFile } = {} } = {} } = patternFile;
+
+  if(templateFile.includes(projectFolder)){
+    errors.push(new ValidationError('Please remove the projectFolder value from the templateFile', null, null, 'gitHub.template.templateURL'))
+  }
+
+  // Check to make sure there is a validate path to this file.
+  if(repoURL && templateFile){
+    const URL = path.join(repoURL, templateFile);
+    console.log('Fetching info from', URL)
+    const response = await fetch(URL);
+    if(response.status !== 200){
+      errors.push(new ValidationError('Failed to find the file template file to load on Serverless Land. Please make sure your GitHub configurtion is correct.', null, null, 'gitHub.template'))
+    }
+  }
+
+  return errors;
+};
+
+const convertToFriendlyMessages = (errors) => {
+  return errors.map((error) => {
+    if (error.includes('.linkedin is of prohibited type [object Object]')) {
+      return error.replace('.linkedin is of prohibited type [object Object]', '.linkedin. Please remove the URL in this property, only include the ID to your LinkedIn profile.');
+    }
+
+    return error;
+  });
+};
 
 const buildErrors = (validationErrors) => {
   return validationErrors.map((error) => {
@@ -32,6 +67,9 @@ const modifiedFiles = process.env.MODIFIED_FILES ? process.env.MODIFIED_FILES.sp
 const findFile = (array, filename) => array.find((item) => item.includes(filename));
 
 const pathToExamplePattern = findFile([...addedFiles, ...modifiedFiles], 'example-pattern.json');
+
+// Run locally...
+// const pathToExamplePattern = path.join('activemq-lambda', 'example-pattern.json');
 
 const main = async () => {
   if (!pathToExamplePattern) {
@@ -68,10 +106,19 @@ const main = async () => {
 
     const result = v.validate(parsedJSON, schema);
 
-    if (result.errors.length > 0) {
-      const errors = buildErrors(result.errors);
+    // Some validation we can't do easily in JSON schema validation, this uses code to validate the pattern
+    const resultsWithCustomValidation = await customValidate(parsedJSON);
+
+    const mergedErrors = [...result.errors, ...resultsWithCustomValidation];
+
+    if (mergedErrors.length > 0) {
+      const errors = buildErrors(mergedErrors);
 
       const errorList = errors.map((error, index) => `${index + 1}. \`${error.path}\`: ${error.stack}\n`);
+
+      const friendlyErrorMessages = convertToFriendlyMessages(errorList);
+
+      console.log('friendlyErrorMessages', friendlyErrorMessages);
 
       if (githubAutomation) {
         // Write comment back with errors for
@@ -81,7 +128,7 @@ const main = async () => {
           issue_number: process.env.PR_NUMBER,
           body:
             `@${process.env.ACTOR} your 'example-pattern.json' is missing some key fields, please review below and address any errors you have \n\n` +
-            `${errorList.reduce((acc, error) => `${acc}${error}`, '')} \n\n` +
+            `${friendlyErrorMessages.reduce((acc, error) => `${acc}${error}`, '')} \n\n` +
             `_If you need any help, take a look at the [example-pattern file](https://github.com/aws-samples/serverless-patterns/blob/main/_pattern-model/example-pattern.json)._ \n\n` +
             `Make the changes, and push your changes back to this pull request. When all automated checks are successful, the Serverless DA team will process your pull request. \n\n`,
         });
