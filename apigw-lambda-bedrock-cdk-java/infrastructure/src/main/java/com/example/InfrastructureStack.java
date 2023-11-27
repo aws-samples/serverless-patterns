2c9d16525912d1498e6576b2c1b2567e158b019e
kozub <kozubg@gmail.com>
2023-11-27 15:29:38 +0100

    Add a pattern from ApiGateway-Lambda-Bedrock using AWS SDK for Java

---
+++ apigw-lambda-bedrock-cdk-java/infrastructure/src/main/java/com/example/InfrastructureStack.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigateway.LambdaIntegration;
+import software.amazon.awscdk.services.apigateway.RestApi;
+import software.amazon.awscdk.services.iam.PolicyStatement;
+import software.amazon.awscdk.services.iam.PolicyStatementProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.constructs.Construct;
+
+import java.util.List;
+
+public class InfrastructureStack extends Stack {
+    public InfrastructureStack(final App parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public InfrastructureStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        // Create policy to allow Lambda call Bedrock
+        PolicyStatement invokeModelPolicy = new PolicyStatement(PolicyStatementProps.builder()
+                .actions(List.of("bedrock:InvokeModel"))
+                .resources(List.of("arn:aws:bedrock:" + getRegion() + "::foundation-model/anthropic.claude-v2"))
+                .build());
+
+
+        // Create a Lambda function
+        Function lambdaFunction = Function.Builder.create(this, "LambdaBedrockFunction")
+                .runtime(Runtime.JAVA_21)
+                .handler("com.example.BedrockClient::handleRequest")
+                .code(Code.fromAsset("../software/target/BedrockClient-1.0.jar"))
+                .timeout(Duration.seconds(60))
+                .initialPolicy(List.of(invokeModelPolicy))
+                .build();
+
+        // Create an API Gateway
+        RestApi api = RestApi.Builder.create(this, "LambdaBedrockAPI")
+                .restApiName("LambdaBedrockAPI")
+                .description("The serverless pattern for calling Bedrock using Java SDK")
+                .build();
+
+        // Create a Lambda integration and connect it to the root resource
+        LambdaIntegration lambdaIntegration = LambdaIntegration.Builder.create(lambdaFunction)
+                .build();
+
+        api.getRoot().addMethod("POST", lambdaIntegration);
+    }
+}
