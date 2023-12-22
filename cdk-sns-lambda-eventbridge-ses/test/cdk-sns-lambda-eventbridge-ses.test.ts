723eb15be8777c6f9745ceafbb5559b633f40c80
Anirudh Sharma <shaniru@amazon.com>
2023-12-22 15:58:07 +0530

    Initial shaniru-feature-cdk-sns-lambda-eventbridge-ses commit

---
+++ cdk-sns-lambda-eventbridge-ses/test/cdk-sns-lambda-eventbridge-ses.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSnsLambdaEventbridgeSes from '../lib/cdk-sns-lambda-eventbridge-ses-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-sns-lambda-eventbridge-ses-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSnsLambdaEventbridgeSes.CdkSnsLambdaEventbridgeSesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
