4266d546807fdc045ff9284543d3985e8e22a629
Biswanath Mukherjee <awsbiswa@amazon.com>
2024-01-22 13:14:19 +0530

    Removed commented out codes

--- fifo-sqs-redrive/redrivenmessage/app.py
+++ fifo-sqs-redrive/redrivenmessage/app.py
@@ -16,26 +16,3 @@ def lambda_handler(event, context):
       QueueUrl=os.environ['QUEUE_URL'],
       ReceiptHandle=record['receiptHandle']
     )
-
-    # # message = json.loads(record['body'])
-
-    # receive_count = int(record["attributes"]["ApproximateReceiveCount"])
-    # message = record["body"]
-        
-    # if receive_count == 1:
-    #     # Raise exception if receive count is 1
-    #     raise Exception('Failed to process message')
-        
-    # # Otherwise process message successfully    
-    # message_data = json.loads(message)
-    # print(f"Processed message: {message_data}")
-    
-    # # Process message but fail on purpose
-    # # if message['id'] % 3 == 0:
-    # # raise Exception('Failed to process message')
-    
-    # # # Delete message if processed successfully 
-    # sqs.delete_message(
-    #   QueueUrl=os.environ['QUEUE_URL'],
-    #   ReceiptHandle=record['receiptHandle']
-    # )
