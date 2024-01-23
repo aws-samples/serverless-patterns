4335f10962b63f442af531b4950067018b442b36
Manikandan Karimanal <32000036+manikandanks2000@users.noreply.github.com>
2024-01-23 16:06:06 +0000

    folder name changes

--- cdk-eventbridge-pipes-sqs-to-stepfunctions-filter-transform-python/app.py
+++ cdk-eventbridge-pipes-sqs-to-stepfunctions-filter-transform-python/app.py
@@ -3,7 +3,7 @@ import os
 
 import aws_cdk as cdk
 
-from eventbridge_pipes_sqs_to_stepfunctions_python.eventbridge_pipes_sqs_to_stepfunctions_python_stack import SqsToStepfunctionsFilterTransformStack
+from src.cdkstack import SqsToStepfunctionsFilterTransformStack
 
 
 app = cdk.App()
