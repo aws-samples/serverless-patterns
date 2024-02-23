219ab6dac5179c374d10c10aba26da869e1db572
Surbhita Shrote <surbhita.git@gmail.com>
2024-02-23 19:54:43 +0530

    added s3-lambda-cdk-dotnet pattern

---
+++ s3-lambda-cdk-dotnet/src/S3Lambda/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace S3Lambda
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new S3LambdaStack(app, "S3LambdaStack");
+            app.Synth();
+        }
+    }
+}
