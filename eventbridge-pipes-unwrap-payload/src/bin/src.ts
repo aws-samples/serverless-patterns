7204ea8e26cb51343ca077c29de680b76589fbc1
Dominik Richter <doricht@amazon.de>
2024-03-28 12:43:31 +0100

    removed obsolete folders

--- eventbridge-pipes-unwrap-payload/src/bin/src.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { UnwrapStack } from '../lib/unwrap-payload-with-pipes';
-
-const app = new cdk.App();
-new UnwrapStack(app, 'UnwrapStack');
