766e4f77b46b3b8c9f76e65bb27a3c14b935e914
ellisms <114107920+ellisms@users.noreply.github.com>
2024-04-04 14:45:57 -0400

    Tagging

--- eventbridge-cloudwatch-cdk-java/src/main/java/com/myorg/EventBridgeToCloudWatchLogsStackApp.java
+++ eventbridge-cloudwatch-cdk-java/src/main/java/com/myorg/EventBridgeToCloudWatchLogsStackApp.java
@@ -7,7 +7,7 @@ public class EventBridgeToCloudWatchLogsStackApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new EventBridgeToCloudWatchLogsStack(app, "EventBridgeToCloudWatchLogsStack", StackProps.builder()
+        new EventBridgeToCloudWatchLogsStack(app, "EventBridgeToCloudWatchLogsStack", StackProps.builder().description("(uksb-1tthgi812) (tag:eventbridge-cloudwatch-cdk-java")
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
