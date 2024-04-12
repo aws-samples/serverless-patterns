3216e4489a61a0725c1a532aa2bf83623eab1db8
Dinesh Balaaji Prabakaran <43818089+pdineshbalaaji@users.noreply.github.com>
2024-04-12 12:18:57 -0400

    Update test_cdk_s3_lambda_sns_python_stack.py

--- cdk-s3-lambda-sns-python/tests/unit/test_cdk_s3_lambda_sns_python_stack.py
+++ cdk-s3-lambda-sns-python/tests/unit/test_cdk_s3_lambda_sns_python_stack.py
@@ -9,7 +9,3 @@ def test_sqs_queue_created():
     app = core.App()
     stack = CdkS3LambdaSnsPythonStack(app, "cdk-s3-lambda-sns-python")
     template = assertions.Template.from_stack(stack)
-
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
