e23391b3846b00f8b7b82ca4c15b3882d10ac4d9
Dinesh Balaaji Prabakaran <43818089+pdineshbalaaji@users.noreply.github.com>
2024-04-12 12:25:42 -0400

    Update test_apigw_vpclink_pvtnlb_fargate_cdk_python_stack.py

--- apigw-vpclink-pvtnlb-fargate-cdk-python/tests/unit/test_apigw_vpclink_pvtnlb_fargate_cdk_python_stack.py
+++ apigw-vpclink-pvtnlb-fargate-cdk-python/tests/unit/test_apigw_vpclink_pvtnlb_fargate_cdk_python_stack.py
@@ -9,7 +9,3 @@ def test_sqs_queue_created():
     app = core.App()
     stack = ApigwVpclinkPvtnlbFargateCdkPythonStack(app, "apigw-vpclink-pvtnlb-fargate-cdk-python")
     template = assertions.Template.from_stack(stack)
-
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
