74034ccb25d2ad021b584222f0569e9f733b38b8
ellisms <114107920+ellisms@users.noreply.github.com>
2024-05-07 04:27:43 -0400

    Tagging

--- appsync-subscription-filter-cdk/app.ts
+++ appsync-subscription-filter-cdk/app.ts
@@ -6,5 +6,5 @@ import { DemoStack } from './lib/demo-stack';
 const app = new cdk.App();
 new DemoStack(app, 'DemoStack', {
     stackName: 'AppSync-Subscription-Enhanced-Filtering-Demo',
-    description: 'Demo stack for the appsync-subscription-filter-cdk pattern',
+    description: 'Demo stack for the appsync-subscription-filter-cdk pattern (uksb-1tthgi812) (tag:appsync-subscription-filter-cdk)',
 });
