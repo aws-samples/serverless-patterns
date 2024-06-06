715ff3cdb8103d57ace1c52e25bb03a272051882
Elie <eliegab@amazon.co.uk>
2024-06-06 18:30:56 +0100

    First implementation, testing crawlers runs and using S3 as an event source for EB

--- s3-eventbridge-gluecrawler/s3_eventbridge_gluecrawler/s3_eventbridge_gluecrawler_stack.py
+++ s3-eventbridge-gluecrawler/s3_eventbridge_gluecrawler/s3_eventbridge_gluecrawler_stack.py
@@ -1,7 +1,11 @@
+import aws_cdk as cdk
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_events as events,
+    aws_iam as iam,
+    aws_glue_alpha as glue_alpha,
+    aws_glue as glue,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
@@ -10,10 +14,48 @@ class S3EventbridgeGluecrawlerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        #S3 buckets
+        input_bucket = s3.Bucket(self, "input-bucket",
+            event_bridge_enabled=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=cdk.RemovalPolicy.DESTROY       
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "S3EventbridgeGluecrawlerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        #Eventbridge rules
+        rule = events.Rule(self, "s3-create-object-event-rule",
+            event_pattern=events.EventPattern(
+                source=["aws.s3"],
+                detail_type=["Object Created"]
+            )
+        )
+
+        #Glue crawler and database
+        glue_db = glue_alpha.Database(self, "s3_eventbridge_glue_db",
+            database_name="s3_eventbridge_glue_db",
+            description="Database to store the results of the glue crawler"
+        )
+
+        input_crawler_role = iam.Role(self, "input-crawler-role",
+            assumed_by=iam.ServicePrincipal("glue.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSGlueServiceRole"),
+            ]
+        )
+
+        input_crawler_role.add_to_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            resources=[f"{input_bucket.bucket_arn}*"],
+            actions=["s3:GetObject", "s3:PutObject"]
+        ))
+
+        input_crawler = glue.CfnCrawler(self, "input-crawler",
+            name="input-crawler",
+            role=input_crawler_role.role_arn,
+            database_name=glue_db.database_name,
+            targets={
+                "s3Targets": [{"path": f"s3://{input_bucket.bucket_name}/"}]
+            }
+        )
+
+        #State machine
+    
