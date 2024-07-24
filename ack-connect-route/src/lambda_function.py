ba4e67e15999d7e5494e98a45b0e0cc732a3da86
Manasvi Jain <jain.manasvi13@gmail.com>
2024-07-24 10:58:59 +0530

    ack-connect-route

---
+++ ack-connect-route/src/lambda_function.py
@@ -0,0 +1,15 @@
+import json 
+import boto3 
+
+lambdaClient = boto3.client('lambda')
+def lambda_handler(event, context):
+    print("Got an event from some route ", event)     
+    if event['requestContext']['routeKey'] == "$connect":  
+         url = "https://" + event["requestContext"]["domainName"] + "/" + event["requestContext"]["stage"] 
+
+    response = lambdaClient.invoke(FunctionName="WebsocketPostToConnectionId",InvocationType="Event",Payload=json.dumps({"url": url, "connectionId":event['requestContext']['connectionId']}))    
+    payload = "This won't be received by the client"    
+    return {'statusCode': 200,'body': payload}
+
+
+
