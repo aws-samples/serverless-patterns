8563078c238bcd9a3521a6b612b3041cfe462c81
Vandita2020 <vandita.vk20@gmail.com>
2024-07-25 14:42:03 -0700

    adding new updates

--- alb-lambda-cdk/lambda/index.ts
+++ alb-lambda-cdk/lambda/index.ts
@@ -1,7 +1,7 @@
 export const handler = async (event: any = {}): Promise<any> => {
   return {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify('Hello from Updtaed Lambda!'),
     headers: {
         'Content-Type': 'application/json'
     }
