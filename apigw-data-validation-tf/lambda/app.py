3805b4d57aacb19ffb5aa607b5cb5f5a2a75be5e
EC2 Default User <ec2-user@ip-172-31-15-245.ec2.internal>
2025-01-12 09:38:58 +0000

    Added new pattern

--- apigw-data-validation-tf/lambda/app.py
+++
@@ -1,9 +0,0 @@
-import json
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Data validation succeded",
-            "data": json.loads(event["body"])
-        }),
-    }
