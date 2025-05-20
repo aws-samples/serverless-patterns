f8ee7d78df78e0f3ac5c540643dda503fa30ae28
onitir <140439279+onitir@users.noreply.github.com>
2025-05-20 12:30:14 -0400

    Delete serverless-patterns-master directory

--- serverless-patterns-master/apigw-bedrock-cognito-terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cognito_user_pool" {
-  value       = aws_cognito_user_pool.UserPool.id
-  description = "CognitoUserPoolID"
-}
-
-output "Endpoint_URL" {
-  value       = aws_api_gateway_stage.stage.invoke_url
-  description = "Root Resource ID for the API"
-}
--- serverless-patterns-master/apigw-bedrock-cognito-terraform/utils/delete_all_api_keys.sh
+++
@@ -1,4 +0,0 @@
-for api_key_id in $(aws apigateway get-api-keys --query 'items[*].id' --output text); do
-    echo  "Deleting API key $api_key_id"
-    aws apigateway delete-api-key --api-key $api_key_id
-done
