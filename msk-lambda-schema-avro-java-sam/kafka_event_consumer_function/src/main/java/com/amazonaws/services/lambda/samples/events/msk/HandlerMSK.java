d6ad9c9506b040ad5e984c0e322fa17de3dd7e7a
Vaibhav Jain <jaivai@amazon.com>
2025-06-22 22:09:38 -0400

    Revert "deleted redundant files"
    This reverts commit 5399dcdc2508b82b4bb33d71fdc803c87e891a83.

---
+++ msk-lambda-schema-avro-java-sam/kafka_event_consumer_function/src/main/java/com/amazonaws/services/lambda/samples/events/msk/HandlerMSK.java
@@ -0,0 +1,275 @@
+//Lambda Runtime delivers a batch of messages to the lambda function
+//Each batch of messages has two fields EventSource and EventSourceARN
+//Each batch of messages also has a field called Records
+//The Records is a map with multiple keys and values
+//Each key is a combination of the Topic Name and the Partition Number
+//One batch of messages can contain messages from multiple partitions
+
+/*
+To simplify representing a batch of Kafka messages as a list of messages
+We have created a Java class called KafkaMessage under the models package
+Here we are mapping the structure of an incoming Kafka event to a list of
+objects of the KafkaMessage class
+*/
+
+package com.amazonaws.services.lambda.samples.events.msk;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.KafkaEvent;
+import com.amazonaws.services.lambda.runtime.events.KafkaEvent.KafkaEventRecord;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class HandlerMSK implements RequestHandler<KafkaEvent, String>{
+	//We initialize an empty list of the KafkaMessage class
+	List<KafkaMessage> listOfMessages = new ArrayList<KafkaMessage>();
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	@Override
+	public String handleRequest(KafkaEvent event, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("========== LAMBDA FUNCTION STARTED ==========");
+		logger.log("Event received: " + gson.toJson(event));
+		
+		String response = new String("200 OK");
+		this.listOfMessages = new ArrayList<KafkaMessage>();
+		
+		// Counters for zip code patterns
+		int zip1000Count = 0;
+		int zip2000Count = 0;
+		//Incoming KafkaEvent object has a property called records that is a map
+		//Each key in the map is a combination of a topic and a partition
+		Map<String, List<KafkaEventRecord>> record=event.getRecords();
+		
+		if (record == null) {
+		    logger.log("WARNING: Event records map is null");
+		    return response;
+		}
+		
+		logger.log("Records map size: " + record.size());
+		
+		Set<String> keySet = record.keySet();
+		logger.log("Key set size: " + keySet.size());
+		logger.log("Keys: " + keySet);
+		  
+		Iterator<String> iterator = keySet.iterator();
+		//We iterate through each of the keys in the map
+		while (iterator.hasNext()) {
+			String thisKey=(String)iterator.next();
+			logger.log("Processing key: " + thisKey);
+			
+			//Using the key we retrieve the value of the map which is a list of KafkaEventRecord
+	    	//One object of KafkaEventRecord represents an individual Kafka message
+			List<KafkaEventRecord>  thisListOfRecords = record.get(thisKey);
+			
+			if (thisListOfRecords == null) {
+			    logger.log("WARNING: Record list for key " + thisKey + " is null");
+			    continue;
+			}
+			
+			logger.log("Record list size for key " + thisKey + ": " + thisListOfRecords.size());
+			
+			//We now iterate through the list of KafkaEventRecords
+			for(KafkaEventRecord thisRecord : thisListOfRecords) {
+				logger.log("Processing record...");
+				
+				/*
+	    		We initialize a new object of the KafkaMessage class which is a simplified representation in our models package
+	    		We then get the fields from each kafka message in the object of KafkaEventRecord class and set them to the fields
+	    		of the KafkaRecord class
+	    		*/
+				KafkaMessage thisMessage = new KafkaMessage();
+				thisMessage.setTopic(thisRecord.getTopic());
+				thisMessage.setPartition(thisRecord.getPartition());
+				thisMessage.setOffset(thisRecord.getOffset());
+				thisMessage.setTimestamp(thisRecord.getTimestamp());
+				thisMessage.setTimestampType(thisRecord.getTimestampType());
+				
+				logger.log("Record metadata - Topic: " + thisRecord.getTopic() + 
+				           ", Partition: " + thisRecord.getPartition() + 
+				           ", Offset: " + thisRecord.getOffset());
+				
+				String key = thisRecord.getKey();
+				String value = thisRecord.getValue();
+				
+				logger.log("Key (base64): " + key);
+				logger.log("Value (base64): " + value);
+				
+				String decodedKey = "null";
+				String decodedValue = "null";
+				//the key and value inside a kafka message are base64 encrypted and will need to be decrypted
+				if (null != key) {
+					logger.log("Decoding key...");
+					try {
+					    byte[] decodedKeyBytes = Base64.getDecoder().decode(key);
+					    decodedKey = new String(decodedKeyBytes);
+					    logger.log("Decoded key: " + decodedKey);
+					} catch (Exception e) {
+					    logger.log("ERROR decoding key: " + e.getMessage());
+					}
+				} else {
+				    logger.log("Key is null");
+				}
+				
+				if (null != value) {
+					logger.log("Decoding value...");
+					try {
+					    byte[] decodedValueBytes = Base64.getDecoder().decode(value);
+					    logger.log("Value decoded, length: " + decodedValueBytes.length + " bytes");
+					    
+					    // Print the complete message in hex format
+					    logger.log("Complete message in hex format:");
+					    logger.log(bytesToHexString(decodedValueBytes, 0));
+					    
+					    try {
+					        decodedValue = new String(decodedValueBytes);
+					        logger.log("Decoded value as string: " + (decodedValue.length() > 100 ? decodedValue.substring(0, 100) + "..." : decodedValue));
+					        
+					        // Add more detailed logging for AVRO messages
+					        logger.log("=== AVRO MESSAGE DETAILS ===");
+					        logger.log("Message appears to be AVRO-formatted. Attempting to extract fields:");
+					        
+					        // Try to extract some common fields from the AVRO binary data
+					        // This is a simple approach to show some readable content
+					        StringBuilder readableContent = new StringBuilder();
+					        for (int i = 0; i < decodedValueBytes.length; i++) {
+					            // Skip non-printable characters
+					            if (decodedValueBytes[i] >= 32 && decodedValueBytes[i] < 127) {
+					                readableContent.append((char)decodedValueBytes[i]);
+					            }
+					        }
+					        
+					        String readableString = readableContent.toString();
+					        logger.log("Readable content extracted from AVRO: " + readableString);
+					        
+					        // Check for zip code patterns
+					        if (readableString.contains("1000")) {
+					            logger.log("FOUND ZIP CODE STARTING WITH 1000");
+					        }
+					        if (readableString.contains("2000")) {
+					            logger.log("FOUND ZIP CODE STARTING WITH 2000");
+					        }
+					        
+					        logger.log("=== END AVRO MESSAGE DETAILS ===");
+					    } catch (Exception e) {
+					        logger.log("ERROR converting bytes to string: " + e.getMessage());
+					        decodedValue = "Error decoding: " + e.getMessage();
+					    }
+					} catch (Exception e) {
+					    logger.log("ERROR decoding value: " + e.getMessage());
+					    e.printStackTrace();
+					}
+				} else {
+				    logger.log("Value is null");
+				}
+				
+				thisMessage.setKey(key);
+	    		thisMessage.setValue(value);
+	    		thisMessage.setDecodedKey(decodedKey);
+	    		thisMessage.setDecodedValue(decodedValue);
+	    		
+	    		//A kafka message can optionally have a list of headers
+	    		//the below code is to get the headers, iterate through each header and get its key and value
+				List<KafkaHeader> headersInThisMessage = new ArrayList<KafkaHeader>();
+				List<Map<String, byte[]>> headers = thisRecord.getHeaders();
+				
+				if (headers != null) {
+				    logger.log("Headers count: " + headers.size());
+				    
+				    for (Map<String, byte[]> thisHeader : headers) {
+					    Set<String> thisHeaderKeys = thisHeader.keySet();
+					    Iterator<String> thisHeaderKeysIterator = thisHeaderKeys.iterator();
+					    while (thisHeaderKeysIterator.hasNext()) {
+						    String thisHeaderKey = thisHeaderKeysIterator.next();
+						    byte[] thisHeaderValue = (byte[])thisHeader.get(thisHeaderKey);
+						    String thisHeaderValueString = new String(thisHeaderValue);
+						    KafkaHeader thisMessageHeader = new KafkaHeader();
+						    thisMessageHeader.setKey(thisHeaderKey);
+						    thisMessageHeader.setValue(thisHeaderValueString);
+						    headersInThisMessage.add(thisMessageHeader);
+						    logger.log("Header - Key: " + thisHeaderKey + ", Value: " + thisHeaderValueString);
+					    }
+				    }
+				} else {
+				    logger.log("No headers in message");
+				}
+				
+				thisMessage.setHeaders(headersInThisMessage);
+				listOfMessages.add(thisMessage);
+				
+				// Below we are logging the particular kafka message in string format using the toString method
+	            // as well as in Json format using gson.toJson function
+				logger.log("Received this message from Kafka - " + thisMessage.toString());
+				logger.log("Message in JSON format : " + gson.toJson(thisMessage));
+				
+				// Add a more readable summary of the message
+				logger.log("=== MESSAGE SUMMARY ===");
+				logger.log("Topic: " + thisMessage.getTopic());
+				logger.log("Partition: " + thisMessage.getPartition());
+				logger.log("Offset: " + thisMessage.getOffset());
+				logger.log("Key: " + thisMessage.getDecodedKey());
+				
+				// Check for zip code patterns in the decoded value
+				String decodedValueStr = thisMessage.getDecodedValue();
+				if (decodedValueStr != null) {
+				    if (decodedValueStr.contains("1000")) {
+				        logger.log("ZIP CODE: Found 1000 pattern in message");
+				        zip1000Count++;
+				    }
+				    if (decodedValueStr.contains("2000")) {
+				        logger.log("ZIP CODE: Found 2000 pattern in message");
+				        zip2000Count++;
+				    }
+				}
+				
+				logger.log("=== END MESSAGE SUMMARY ===");
+			}
+		}
+		logger.log("All Messages in this batch = " + gson.toJson(listOfMessages));
+		
+		// Log summary of zip code distribution
+		logger.log("========== ZIP CODE DISTRIBUTION SUMMARY ==========");
+		logger.log("Messages with zip code containing 1000: " + zip1000Count);
+		logger.log("Messages with zip code containing 2000: " + zip2000Count);
+		logger.log("Other messages: " + (listOfMessages.size() - zip1000Count - zip2000Count));
+		logger.log("====================================================");
+		
+		logger.log("========== LAMBDA FUNCTION COMPLETED ==========");
+		return response;
+	}
+	
+	/**
+     * Convert byte array to hexadecimal string representation
+     * 
+     * @param bytes Byte array to convert
+     * @param maxLength Maximum number of bytes to convert (0 for all)
+     * @return Hexadecimal string representation
+     */
+    private String bytesToHexString(byte[] bytes, int maxLength) {
+        StringBuilder sb = new StringBuilder();
+        int length = maxLength > 0 && maxLength < bytes.length ? maxLength : bytes.length;
+        
+        for (int i = 0; i < length; i++) {
+            sb.append(String.format("%02X", bytes[i]));
+            if (i % 16 == 15) {
+                sb.append("\n");
+            } else if (i % 4 == 3) {
+                sb.append(" ");
+            }
+        }
+        
+        if (maxLength > 0 && length < bytes.length) {
+            sb.append("... (").append(bytes.length - length).append(" more bytes)");
+        }
+        
+        return sb.toString();
+    }
+}
