7d0f3b0c9275192e06bebfa10be3d59280796706
kakakakakku <y.yoshida22@gmail.com>
2025-08-12 22:49:27 +0900

    eventbridge-sns-lambda-terraform: Remove unused code

--- eventbridge-sns-lambda-terraform/src/app.js
+++ eventbridge-sns-lambda-terraform/src/app.js
@@ -2,10 +2,6 @@
  *  SPDX-License-Identifier: MIT-0
  */
 
-const AWS = require('aws-sdk')
-AWS.config.region = process.env.AWS_REGION 
-const sns = new AWS.SNS({apiVersion: '2012-11-05'})
-
 // The Lambda handler
 exports.handler = async (event) => {
   console.log("Hello World !")
