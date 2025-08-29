d3080b8369c636d35ffc2f120279479ad7d9a5f8
kakakakakku <y.yoshida22@gmail.com>
2025-08-29 19:36:31 +0900

    lambda-eventbridge-terraform: Fix deprecated `managed_policy_arns` argument

--- lambda-eventbridge-terraform/main.tf
+++ lambda-eventbridge-terraform/main.tf
@@ -43,11 +43,7 @@ data "aws_iam_policy" "lambda_basic_execution_role_policy" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name_prefix         = "PublisherFunctionRole-"
-  managed_policy_arns = [
-    data.aws_iam_policy.lambda_basic_execution_role_policy.arn,
-    aws_iam_policy.event_bridge_put_events_policy.arn
-  ]
+  name_prefix = "PublisherFunctionRole-"
 
   assume_role_policy = <<EOF
 {
@@ -64,6 +60,16 @@ resource "aws_iam_role" "iam_for_lambda" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = data.aws_iam_policy.lambda_basic_execution_role_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_eventbridge" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.event_bridge_put_events_policy.arn
 }
 
 data "aws_iam_policy_document" "event_bridge_put_events_policy_document" {
