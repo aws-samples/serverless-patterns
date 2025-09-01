ff919ccadba2764a9ce987d0e937f667ffa40f53
kakakakakku <y.yoshida22@gmail.com>
2025-09-01 10:29:27 +0900

    s3-lambda-terraform: Update runtime to nodejs22.x

--- s3-lambda-terraform/main.tf
+++ s3-lambda-terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 5.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -32,7 +32,7 @@ resource "aws_lambda_function" "lambda_s3_handler" {
   source_code_hash = data.archive_file.lambda_zip_file.output_base64sha256
   handler          = "index.handler"
   role             = aws_iam_role.iam_for_lambda.arn
-  runtime          = "nodejs16.x"
+  runtime          = "nodejs22.x"
 }
 
 data "archive_file" "lambda_zip_file" {
