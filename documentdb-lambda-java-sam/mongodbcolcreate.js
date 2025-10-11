158fef3604b3de90cfafa246297b60d660d3d51c
Indranil Banerjee <ibanerj@amazon.com>
2025-10-11 10:55:14 -0700

    Fixed DocumentDB pattern CloudFormation issues

--- documentdb-lambda-java-sam/mongodbcolcreate.js
+++ documentdb-lambda-java-sam/mongodbcolcreate.js
@@ -1,3 +1,5 @@
 db = db.getSiblingDB('DOCDB_DATABASE');
 db.createCollection('DOCDB_COLLECTION');
 print("Collection created successfully.");
+
+db.adminCommand({modifyChangeStreams: 1, database: "DOCDB_DATABASE", collection: "DOCDB_COLLECTION", enable: true});
