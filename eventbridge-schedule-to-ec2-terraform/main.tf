de9929177637f3e8a338294445055f949daa7627
kakakakakku <y.yoshida22@gmail.com>
2026-01-22 09:16:33 +0900

    eventbridge-schedule-to-ec2-terraform: Fix "Most Recent Image Not Filtered" error

--- eventbridge-schedule-to-ec2-terraform/main.tf
+++ eventbridge-schedule-to-ec2-terraform/main.tf
@@ -34,14 +34,10 @@ resource "aws_subnet" "subnet" {
 
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
-    name = "owner-alias"
-    values = ["amazon"]
-  }
-  
-  filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
 }
