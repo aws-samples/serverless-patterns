1c0b95335dedbf1590ab3be96233ea5e1dc5b105
archiev4 <archie.v4.11@gmail.com>
2026-03-09 11:36:41 +0530

    Updated version

--- eventbridge-fanout-pattern/main.tf
+++ eventbridge-fanout-pattern/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.5"
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.32.1"
     }
   }
 }
