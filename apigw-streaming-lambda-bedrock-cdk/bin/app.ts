aa5a6c5ede0b97aa783ac185435649fa97c29d82
ellisms <114107920+ellisms@users.noreply.github.com>
2026-05-08 08:40:11 -0400

    pattern tagging

--- apigw-streaming-lambda-bedrock-cdk/bin/app.ts
+++ apigw-streaming-lambda-bedrock-cdk/bin/app.ts
@@ -3,5 +3,6 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { ApigwStreamingLambdaBedrockStack } from "../lib/apigw-streaming-lambda-bedrock-stack";
 
+const description="Sample app (uksb-1tthgi812) (tag:apigw-streaming-lambda-bedrock-cdk"
 const app = new cdk.App();
-new ApigwStreamingLambdaBedrockStack(app, "ApigwStreamingLambdaBedrockStack");
+new ApigwStreamingLambdaBedrockStack(app, "ApigwStreamingLambdaBedrockStack",{description});
