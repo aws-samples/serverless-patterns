431e3eec2ac9ac596c6e4605371b3894c71addde
Lavanya Tangutur <lavatang@amazon.com>
2026-06-01 22:49:49 -0400

    removed old directory

--- apigw-dynamodb-tenantid-cdk/deploy_dynamodb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-APP="npx ts-node --prefer-ts-exts bin/apigw-dynamodb-apikey-cdk.ts"
-STACK_NAME="ApigwDynamodbApikeyCdkStack"
-
-npm install
-cdk deploy "$STACK_NAME" --app "$APP" "$@"
